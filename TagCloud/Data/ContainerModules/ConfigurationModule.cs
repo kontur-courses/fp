2e07ee25e4337e2b9a18058f97b5eb539a4c335f
Artemiy <38810090+CGOptimum@users.noreply.github.com>
2018-12-18 19:00:41 +0500

    autofac modules added

---
+++ TagCloud/Data/ContainerModules/ConfigurationModule.cs
@@ -0,0 +1,34 @@
+﻿using System.Drawing;
+using Autofac;
+using TagCloud.ColorPicker;
+using TagCloud.Counter;
+using TagCloud.Drawer;
+using TagCloud.RectanglesLayouter;
+using TagCloud.RectanglesLayouter.PointsGenerator;
+using TagCloud.Saver;
+using TagCloud.WordsLayouter;
+
+namespace TagCloud.Data.ContainerModules
+{
+    public class ConfigurationModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<TagCloudGenerator>().AsSelf();
+
+            builder.RegisterModule<ReadersModule>();
+
+            builder.RegisterType<CloudWordsLayouter>().As<IWordsLayouter>();
+            builder.RegisterType<CloudDrawer>().As<IWordsDrawer>();
+            builder.RegisterType<WordsCounter>().As<IWordsCounter>();
+            builder.RegisterType<FileImageSaver>().As<IImageSaver>();
+            builder.RegisterType<ClipboardImageSaver>().As<IImageSaver>();
+
+            builder.RegisterType<CircularCloudLayouter>().As<IRectangleLayouter>();
+            builder.Register(c => new Point()).As<Point>();
+            builder.Register(c => new SpiralPointsGenerator(1, 0.01)).As<IPointsGenerator>();
+
+            builder.RegisterType<BrightnessColorPicker>().As<IColorPicker>();
+        }
+    }
+}
---
+++ TagCloud/Data/ContainerModules/ReadersModule.cs
@@ -0,0 +1,15 @@
+﻿using Autofac;
+using TagCloud.Reader;
+using TagCloud.Reader.FormatReader;
+
+namespace TagCloud.Data.ContainerModules
+{
+    public class ReadersModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<TextFileReader>().As<IWordsFileReader>();
+            builder.RegisterType<DocxReader>().As<IFormatReader>();
+        }
+    }
+}
