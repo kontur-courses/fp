043127cd63207d8f61159e23ae3f97945d4094a0
Vicheslav <rozentor2@gmail.com>
2018-12-18 20:33:50 +0500

    Removed TesBase from tests

--- TagCloud.Test/TestBase.cs
+++ TagCloud.Test/TestBase.cs
@@ -1,10 +1,8 @@
 ﻿using Autofac;
-using NUnit.Framework;
 using TagCloud.Utility.Container;
 
 namespace TagCloud.Tests
 {
-    [TestFixture]
     public class TestBase
     {
         protected readonly IContainer container = ContainerConfig.StandartContainer;
