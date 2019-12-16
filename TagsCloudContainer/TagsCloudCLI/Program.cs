763aa8d864b1cf9c5169beaae20d9b0b611bfea9
Fedor Barkov <hitsedesen@yandex.ru>
2019-12-16 13:22:59 +0500

    Use Enum.TryParse to parse PartsOfSpeech config

--- TagsCloudContainer/TagsCloudCLI/Program.cs
+++ TagsCloudContainer/TagsCloudCLI/Program.cs
@@ -149,50 +149,9 @@ namespace TagsCloudCLI
             var posWhitelist = new List<Word.PartOfSpeech>();
             foreach (var pos in partsOfSpeechWhitelist)
             {
-                switch (pos)
+                if (Enum.TryParse(pos, out Word.PartOfSpeech partOfSpeech))
                 {
-                    case "adjective": 
-                        posWhitelist.Add(Word.PartOfSpeech.Adjective);
-                        break;
-                    case "adverb": 
-                        posWhitelist.Add(Word.PartOfSpeech.Adverb); 
-                        break;
-                    case "adverb-pronoun": 
-                        posWhitelist.Add(Word.PartOfSpeech.AdverbPronoun); 
-                        break;
-                    case "adjective-numeral": 
-                        posWhitelist.Add(Word.PartOfSpeech.AdjectiveNumeral); 
-                        break;
-                    case "adjective-pronoun": 
-                        posWhitelist.Add(Word.PartOfSpeech.AdjectivePronoun); 
-                        break;
-                    case "composite": 
-                        posWhitelist.Add(Word.PartOfSpeech.Composite); 
-                        break;
-                    case "conjunction": 
-                        posWhitelist.Add(Word.PartOfSpeech.Conjunction); 
-                        break;
-                    case "interjection": 
-                        posWhitelist.Add(Word.PartOfSpeech.Interjection); 
-                        break;
-                    case "numeral": 
-                        posWhitelist.Add(Word.PartOfSpeech.Numeral); 
-                        break;
-                    case "particle": 
-                        posWhitelist.Add(Word.PartOfSpeech.Particle); 
-                        break;
-                    case "preposition": 
-                        posWhitelist.Add(Word.PartOfSpeech.Preposition);
-                        break;
-                    case "noun": 
-                        posWhitelist.Add(Word.PartOfSpeech.Noun); 
-                        break;
-                    case "noun-pronoun": 
-                        posWhitelist.Add(Word.PartOfSpeech.NounPronoun);
-                        break;
-                    case "verb": 
-                        posWhitelist.Add(Word.PartOfSpeech.Verb);
-                        break;
+                    posWhitelist.Add(partOfSpeech);
                 }
             }
 
