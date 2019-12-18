99f91ae1d6b658fd6448ea76a2e47c429e413ab3
Nicole <mrhomka.com@gmail.com>
2019-12-18 23:55:27 +0500

    Container in other metod

--- TagsCloudContainer/Options.cs
+++ TagsCloudContainer/Options.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommandLine;
 
 namespace TagsCloudContainer
 {
-    class Options
+    public class Options
     {
+        [Option('r', "read", MetaValue = "FILE", Required = true, HelpText = "Full wordlist file name.")]
+        public string File { get; set; }
+        [Option('h', "height", Default = 1024, HelpText = "Image height.")]
+        public int Height { get; set; }
+        [Option('w', "width", Default = 1024, HelpText = "Image width.")]
+        public int Width { get; set; }
+        [Option('f', "font", Default = "Arial", HelpText = "Font name.")]
+        public string Font { get; set; }
+        [Option('s', "size", Default = 20, HelpText = "Font size.")]
+        public int Size { get; set; }
+        [Option('c', "color", Default = "Red", HelpText = "Font color. (Does not work)")]
+        public string Color { get; set; }
+        [Option('i', "image", Default = "image.png", HelpText = "Image name.")]
+        public string Image { get; set; }
     }
 }
