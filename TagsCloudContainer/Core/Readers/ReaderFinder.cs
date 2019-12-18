2aff816da398f443ee352be864aedfd15b963e33
lgnv <43182296+lgnv@users.noreply.github.com>
2019-12-18 18:59:37 +0500

    Создан класс, способный по пути к файлу найти Reader способный его прочитать

---
+++ TagsCloudContainer/Core/Readers/ReaderFinder.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+
+namespace TagsCloudContainer.Core.Readers
+{
+    class ReaderFinder
+    {
+        private readonly IReader[] readers;
+        public ReaderFinder(IReader[] readers)
+        {
+            this.readers = readers;
+        }
+
+        public IReader Find(string filePath) => readers.FirstOrDefault(r => r.CanRead(filePath));
+    }
+}
