7333e00845c4dd7c5183b54340a76c8005395a6e
HenrickBolender <40788127+HenrickBolender@users.noreply.github.com>
2019-12-18 23:42:34 +0500

    added result support to check if text is accessible

--- TagsCloudVisualization/TagsCloudVisualization/Logic/TextRetriever.cs
+++ TagsCloudVisualization/TagsCloudVisualization/Logic/TextRetriever.cs
@@ -1,4 +1,6 @@
 ﻿using System.IO;
+using System.Security.AccessControl;
+using System.Security.Principal;
 using System.Text;
 using ErrorHandler;
 
@@ -7,17 +9,39 @@ namespace TagsCloudVisualization.Logic
     public static class TextRetriever
     {
         public static readonly Encoding DefaultEncoding = Encoding.UTF8;
+
         public static Result<string> RetrieveTextFromFile(string path, Encoding encoding)
         {
             if (path == null)
                 return Result.Fail<string>("Path is null");
             if (!File.Exists(path))
                 return Result.Fail<string>($"Path {path} doesn't exist");
+            if (!IsFileAccessible(path))
+                return Result.Fail<string>($"Couldn't get acces to file {path}'");
             var text = File.ReadAllText(path, encoding);
             return text;
         }
+        
+        public static Result<string> RetrieveTextFromFile(string path)
+        {
+            return RetrieveTextFromFile(path, DefaultEncoding);
+        }
 
-        public static Result<string> RetrieveTextFromFile(string path) =>
-            RetrieveTextFromFile(path, DefaultEncoding);
+        private static bool IsFileAccessible(string path)
+        {
+            var fileSecurity = File.GetAccessControl(path);
+            var accessRules = fileSecurity.GetAccessRules(true, true, typeof(NTAccount));
+            foreach (var accessRule in accessRules)
+            {
+                var fileSystemRule = (FileSystemAccessRule) accessRule;
+                if (fileSystemRule.AccessControlType == AccessControlType.Deny &&
+                    (fileSystemRule.FileSystemRights == FileSystemRights.FullControl
+                     || fileSystemRule.FileSystemRights == FileSystemRights.Read
+                     || fileSystemRule.FileSystemRights == FileSystemRights.ReadAndExecute
+                     || fileSystemRule.FileSystemRights == FileSystemRights.Modify))
+                    return false;
+            }
+            return true;
+        }
     }
 }
