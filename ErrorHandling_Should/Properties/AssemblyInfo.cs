9db2af3f85dda15ed5d392eff7bb756a31d37cd7
IAlmostDeveloper <51601917+IAlmostDeveloper@users.noreply.github.com>
2019-12-19 18:18:25 +0500

    updated solution projects

--- ErrorHandling_Should/Properties/AssemblyInfo.cs
+++ ErrorHandling_Should/Properties/AssemblyInfo.cs
@@ -1,16 +1,15 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ErrorHandling")]
+[assembly: AssemblyTitle("ErrorHandling_Should")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ErrorHandling")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("ErrorHandling_Should")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +19,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("c83b0a70-dba6-45bf-a792-fd2b14528ebb")]
+[assembly: Guid("B3DD9D55-207E-4317-B30E-4FB3006C612C")]
 
 // Version information for an assembly consists of the following four values:
 //
