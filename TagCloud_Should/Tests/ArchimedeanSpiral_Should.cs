9e80b0be25e1fc8010204395e313b8b2e9eff1b5
IAlmostDeveloper <51601917+IAlmostDeveloper@users.noreply.github.com>
2019-12-19 18:13:59 +0500

    updated tag cloud unit tests

---
+++ TagCloud_Should/Tests/ArchimedeanSpiral_Should.cs
@@ -0,0 +1,31 @@
+﻿using System.Drawing;
+using System.Linq;
+using FluentAssertions;
+using NUnit.Framework;
+using TagCloud.CloudLayouter.CircularLayouter;
+
+namespace TagCloud_Should.Tests
+{
+    public class ArchimedeanSpiral_Should
+    {
+        private ArchimedeanSpiral spiral;
+
+        [SetUp]
+        public void SetNewSpiral()
+        {
+            spiral = new ArchimedeanSpiral(new SpiralSettings());
+        }
+
+        [Test]
+        public void GetNewPointLazy_ShouldReturnZeroZeroAtStart()
+        {
+            spiral.GetNewPointLazy().GetEnumerator().Current.Should().BeEquivalentTo(new Point(0, 0));
+        }
+
+        [Test]
+        public void GetNewPointLazy_ShouldReturnCorrectPoint()
+        {
+            spiral.GetNewPointLazy().First().Should().BeEquivalentTo(new Point(7, 3));
+        }
+    }
+}
---
+++ TagCloud_Should/Tests/CircularCloudLayouter_Should.cs
@@ -0,0 +1,89 @@
+﻿using System.Drawing;
+using FluentAssertions;
+using NUnit.Framework;
+using TagCloud.CloudLayouter.CircularLayouter;
+using TagCloud.ErrorHandler;
+using TagCloud.Visualization;
+
+namespace TagCloud_Should.Tests
+{
+    [TestFixture]
+    public class CircularCloudLayouter_Should
+    {
+        private CircularCloudLayouter layouter;
+        private SpiralSettings spiralSettings;
+
+        [SetUp]
+        public void SetNewSettings()
+        {
+            spiralSettings = new SpiralSettings();
+        }
+
+//        [TestCase(-1, -1)]
+//        [TestCase(-1, 1)]
+//        [TestCase(1, -1)]
+//        [TestCase(0, 0)]
+//        [TestCase(0, 1)]
+//        [TestCase(1, 0)]
+//        public void NonPositiveLayouterSize_ThrowsException(int width, int height)
+//        {
+//            var imageSettings = new ImageSettings(new ErrorHandler())
+//                {ImageSize = new Size(width, height)};
+//            Action action = () =>
+//                layouter = new CircularCloudLayouter(new ArchimedeanSpiral(spiralSettings), imageSettings);
+//            action.Should().Throw<ArgumentException>();
+//        }
+
+        [Test]
+        public void PutNextRectangle_FirstRect_ShouldBeInCenterWithSomeBias()
+        {
+            var imageSettings = new ImageSettings(new ErrorHandler())
+                {ImageSize = new Size(600, 600)};
+            layouter = new CircularCloudLayouter(new ArchimedeanSpiral(spiralSettings), imageSettings);
+            var rectangle = layouter.PutNextRectangle(new Size(10, 3), out _).GetValueOrThrow();
+            rectangle.Location.Should().BeEquivalentTo(new Point(7, 3));
+        }
+
+        [TestCase(-1, -1)]
+        [TestCase(-1, 1)]
+        [TestCase(1, -1)]
+        [TestCase(0, 0)]
+        [TestCase(0, 1)]
+        [TestCase(1, 0)]
+        public void PutNextRectangle_NonPositiveRectSize_ResultIsNotSuccess(int width, int height)
+        {
+            var imageSettings = new ImageSettings(new ErrorHandler())
+                {ImageSize = new Size(600, 600)};
+            layouter = new CircularCloudLayouter(new ArchimedeanSpiral(spiralSettings), imageSettings);
+            layouter.PutNextRectangle(new Size(width, height), out _).IsSuccess.Should().BeFalse();
+        }
+
+        [TestCase(101, 50, 100, 100)]
+        [TestCase(100, 101, 200, 100)]
+        [TestCase(101, 51, 100, 50)]
+        public void PutNextRectangle_SizeBiggerThanScreen_ReturnsRightError(int width, int height, int screenWidth,
+            int screenHeight)
+        {
+            var imageSettings = new ImageSettings(new ErrorHandler())
+                {ImageSize = new Size(screenWidth, screenHeight)};
+            layouter = new CircularCloudLayouter(new ArchimedeanSpiral(spiralSettings), imageSettings);
+            layouter
+                .PutNextRectangle(new Size(width, height), out _).Error.Should()
+                .BeEquivalentTo("Rectangle is out of layouter");
+        }
+
+        [Test]
+        public void PutNextRectangle_PositionOutOfScreen_ReturnsRightError()
+        {
+            var imageSettings = new ImageSettings(new ErrorHandler())
+                {ImageSize = new Size(100, 100)};
+            layouter = new CircularCloudLayouter(new ArchimedeanSpiral(spiralSettings), imageSettings);
+            for (var i = 0; i < 50; i++)
+                layouter.PutNextRectangle(new Size(10, 10), out _);
+            layouter.PutNextRectangle(new Size(10, 10), out _)
+                .Error
+                .Should()
+                .BeEquivalentTo("Rectangle is out of layouter");
+        }
+    }
+}
