b06142b0f67375a3180fec092cd4bc198c9b1141
IAlmostDeveloper <51601917+IAlmostDeveloper@users.noreply.github.com>
2019-12-19 18:15:53 +0500

    added extensions

---
+++ TagCloud/Extensions/EnumerableExtension.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using ErrorHandling;
+
+namespace TagCloud.Extensions
+{
+    public static class EnumerableExtension
+    {
+        public static Result<None> ToOneResult(this IEnumerable enumerable)
+        {
+            foreach (var element in enumerable)
+            {
+                if (element is Result<None> result)
+                    if (!result.IsSuccess)
+                        return Result.Fail<None>(result.Error);
+            }
+
+            return Result.Ok<None>(null);
+        }
+    }
+}
