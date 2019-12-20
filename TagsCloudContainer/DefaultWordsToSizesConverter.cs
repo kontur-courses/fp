98fe26e4a4f48e21288cab0797dd43043fa45da9
Zakepanka <arzybek@users.noreply.github.com>
2019-12-20 22:43:21 +0500

    Refactored DWordsToSizesConv

--- TagsCloudContainer/DefaultWordsToSizesConverter.cs
+++ TagsCloudContainer/DefaultWordsToSizesConverter.cs
@@ -9,25 +9,25 @@ namespace TagsCloudContainer
 {
     public class DefaultWordsToSizesConverter : IWordsToSizesConverter
     {
-        public Size Size { get; set; }
+        public Size LayoutSize { get; set; }
         public int MaxHeight { get; set; }
         public int MaxWidth { get; set; }
         private Graphics graphics;
 
-        public DefaultWordsToSizesConverter(Size size, int maxHeight = 0, int maxWidth = 0)
+        public DefaultWordsToSizesConverter(Size layoutSize, int maxHeight = 0, int maxWidth = 0)
         {
-            Size = size;
+            LayoutSize = layoutSize;
             if (maxHeight == 0)
-                MaxHeight = size.Height;
+                MaxHeight = layoutSize.Height;
             if (maxWidth == 0)
-                MaxWidth = size.Width;
+                MaxWidth = layoutSize.Width;
             if (maxHeight != 0 && maxWidth != 0)
             {
                 MaxHeight = maxHeight;
                 MaxWidth = maxWidth;
             }
 
-            var bitmap = new Bitmap(size.Width, size.Height);
+            var bitmap = new Bitmap(layoutSize.Width, layoutSize.Height);
             Graphics g = Graphics.FromImage(bitmap);
             graphics = g;
         }
@@ -41,34 +41,55 @@ namespace TagsCloudContainer
             return new Size(Math.Min((int) widthFD, MaxWidth),
                 Math.Min((int) heightFD, MaxHeight));
         }
+        
+        private double GetSquare(IEnumerable<Size> sizes)
+        {
+            var result = 0.0;
+            foreach (var rect in sizes)
+            {
+                result += rect.Height * rect.Width;
+            }
 
+            return result;
+        }
+
+        private IEnumerable<(string, Size)> FormSizes(Dictionary<string, int> dict)
+        {
+            var res = new List<(string, Size)>();
+            foreach (var key in dict.Keys)
+            {
+                var tup = (key, GetSizeOf(key, dict));
+                res.Add(tup);
+            }
+
+            return res;
+        }
+
+        private double GetCoefficientOfLayoutSquareTo(double actualSquare)
+        {
+            double bitmapSquare = (LayoutSize.Height - LayoutSize.Height / 2.5) * (LayoutSize.Width - LayoutSize.Width / 2.5);
+            var coeff = Math.Sqrt(bitmapSquare / actualSquare);
+            return coeff;
+        }
+        
+        public static Size CalcualteSizeMultiply(double coeff, Size size)
+        {
+            return new Size((int) (size.Width * coeff), (int) (size.Height * coeff));
+        }
+        
         public Result<IEnumerable<(string, Size)>> GetSizesOf(Dictionary<string, int> dictionary)
         {
             return Result.Of(() =>
                 {
-                    var res = new List<(string, Size)>();
-                    foreach (var key in dictionary.Keys)
-                    {
-                        var tup = (key, GetSizeOf(key, dictionary));
-                        res.Add(tup);
-                    }
-
-                    var ourSquare = 0.0;
-                    foreach (var item in res)
-                    {
-                        var rect = item.Item2;
-                        ourSquare += rect.Height * rect.Width;
-                    }
-
-                    double bitmapSquare = (Size.Height - Size.Height / 2.5) * (Size.Width - Size.Width / 2.5);
-                    var coeff = Math.Sqrt(bitmapSquare / ourSquare);
+                    var res = FormSizes(dictionary);
+                    var ourSquare = GetSquare(res.Select(x => x.Item2));
+                    var coeff = GetCoefficientOfLayoutSquareTo(ourSquare);
                     var result = new List<(string, Size)>();
                     foreach (var item in res)
                     {
-                        var newRect = new Size((int) (item.Item2.Width * coeff), (int) (item.Item2.Height * coeff));
+                        var newRect = CalcualteSizeMultiply(coeff, item.Item2);
                         result.Add((item.Item1, newRect));
                     }
-
                     return (IEnumerable<(string, Size)>) result;
                 }, "Something wrong with your size layout " +
                    "and words count. Maybe there are too " +
--- TagsCloudContainer/Interfaces/IWordsToSizesConverter.cs
+++ TagsCloudContainer/Interfaces/IWordsToSizesConverter.cs
@@ -6,7 +6,7 @@ namespace TagsCloudContainer.Interfaces
 {
     public interface IWordsToSizesConverter
     {
-        Size Size { get; set; }
+        Size LayoutSize { get; set; }
         int MaxHeight { get; set; }
         int MaxWidth { get; set; }
         Result<IEnumerable<(string, Size)>> GetSizesOf(Dictionary<string, int> dictionary);
