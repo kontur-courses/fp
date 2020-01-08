5159a4d1703f9a0f1eff599cbbfc623c6636d986
balandin228 <55559705+balandin228@users.noreply.github.com>
2020-01-08 15:54:33 +0500

    add IFormatReader

---
+++ TagCloud/Services/IFormatReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using ResultOf;
+
+namespace TagCloud.Services
+{
+    public interface IFormatReader
+    {
+        Result<ImageFormat> ReadFormat(Dictionary<string, ImageFormat> availableFormats);
+    }
+}
---
+++ TagCloud/Services/PictureFormatReader.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ResultOf;
+using TagCloud.Models;
+
+namespace TagCloud.Services
+{
+    public class PictureFormatReader : IFormatReader
+    {
+        public Result<ImageFormat> ReadFormat(Dictionary<string, ImageFormat> availableFormats)
+        {
+            var defaultFormatName = UserSettings.DefaultSettings.ImageSettings.FontName;
+            Console.WriteLine("Введите формат в котором хотите сохранить");
+            Console.WriteLine("Список доступных форматов :");
+            foreach (var format in availableFormats) Console.WriteLine(format.Key);
+            Console.WriteLine("Оставьте строку пустой, чтоб использовать формат : " + defaultFormatName);
+            Console.Write(">>>");
+            var name = Console.ReadLine();
+            name = name == string.Empty
+                ? defaultFormatName
+                : name;
+            return Result.Of(() =>
+            {
+                if (availableFormats.TryGetValue(name,out var format)) return format;
+                throw new ArgumentException("Введенный формат не поддреживается");
+            });
+        }
+    }
+}
