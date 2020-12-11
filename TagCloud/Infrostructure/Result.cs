a8d1c5b0350e27f851928563ecbd17b7f44179bb
TerribleVoice <terriblevoice@ya.ru>
2020-12-11 15:40:15 +0500

    добавил класс Result

---
+++ TagCloud/Infrostructure/Result.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace WordCloudGenerator
+{
+    public struct Result<T>
+    {
+        public Result(string error, T value = default)
+        {
+            Error = error;
+            Value = value;
+        }
+
+        public static implicit operator Result<T>(T v)
+        {
+            return Result.Ok(v);
+        }
+
+        public string Error { get; }
+        internal T Value { get; }
+
+        public T GetValueOrThrow()
+        {
+            if (IsSuccess) return Value;
+            throw new InvalidOperationException($"No value. Only Error {Error}");
+        }
+
+        public bool IsSuccess => Error == null;
+    }
+
+    public static class Result
+    {
+        public static Result<T> RepeatUntilOk<T>(Func<T> f, Action<string> errorHandler)
+        {
+            Result<T> result;
+            do
+            {
+                result = Of(f);
+                result.OnFail(errorHandler);
+            } while (!result.IsSuccess);
+
+            return result;
+        }
+
+        public static Result<T> Ok<T>(T value)
+        {
+            return new Result<T>(null, value);
+        }
+
+        public static Result<T> Of<T>(Func<T> f, string error = null)
+        {
+            try
+            {
+                return Ok(f());
+            }
+            catch (Exception e)
+            {
+                return Fail<T>(error ?? e.Message);
+            }
+        }
+
+        public static Result<TInput> OnFail<TInput>(
+            this Result<TInput> input,
+            Action<string> handleError)
+        {
+            if (!input.IsSuccess) handleError(input.Error);
+            return input;
+        }
+
+        private static Result<T> Fail<T>(string e)
+        {
+            return new Result<T>(e);
+        }
+    }
+}
