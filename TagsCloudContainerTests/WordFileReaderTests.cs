6038d09f0a141180cb086364efa7ebfe80be8422
catsAndSuicide <anisa.hamidullina00@gmail.com>
2020-12-12 01:51:52 +0500

    added new tests "DocxReader_ShouldReturnResultWithError_IfFileDoesNotExist", "DocxReader_ShouldReturnResultWithError_IfCannotReadFile" in WordFileReaderTests

--- TagsCloudContainerTests/WordFileReaderTests.cs
+++ TagsCloudContainerTests/WordFileReaderTests.cs
@@ -11,15 +11,38 @@ namespace TagsCloudContainerTests
         private readonly string docxFilePath = Path.Combine(Directory.GetCurrentDirectory(),
             "files", "input.docx");
 
+        private readonly string txtFilePath = Path.Combine(Directory.GetCurrentDirectory(),
+            "files", "input.txt");
+
         [Test]
-        public void WordReader_ShouldReadLines()
+        public void WordReader_ShouldReadLines_IfFileExists()
         {
-            new WordFileReader(docxFilePath)
-                .ReadLines()
-                .GetValueOrThrow()
+            var result = new WordFileReader(docxFilePath).ReadLines();
+            result.IsSuccess.Should().BeTrue();
+            result.GetValueOrThrow()
                 .ToArray()
                 .Should()
                 .BeEquivalentTo("Это", "Docx", "Файл", "");
+        }
+
+        [Test]
+        public void DocxReader_ShouldReturnResultWithError_IfFileDoesNotExist()
+        {
+            new WordFileReader("notExistedPath.docx")
+                .ReadLines()
+                .Error
+                .Should()
+                .BeEquivalentTo("Input file is not found");
+        }
+
+        [Test]
+        public void DocxReader_ShouldReturnResultWithError_IfCannotReadFile()
+        {
+            new WordFileReader(txtFilePath)
+                .ReadLines()
+                .Error
+                .Should()
+                .Contain("Can't read input file");
         }
     }
 }
