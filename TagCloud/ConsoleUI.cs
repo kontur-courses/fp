e4c8a0fac7844b6b429c8ff6d982f7afd811ccaf
TerribleVoice <terriblevoice@ya.ru>
2020-12-11 15:42:43 +0500

    переделал под Result и отрефакторил

--- TagCloud/ConsoleUI.cs
+++ TagCloud/ConsoleUI.cs
@@ -1,4 +1,4 @@
-﻿﻿using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
@@ -7,11 +7,12 @@ namespace WordCloudGenerator
 {
     public class ConsoleUI : IUserInterface
     {
-        private readonly IPreparer.Factory preparerFactory;
-        private readonly IPalette.Factory paletteFactory;
         private readonly IPainter.Factory painterFactory;
+        private readonly IPalette.Factory paletteFactory;
+        private readonly IPreparer.Factory preparerFactory;
 
-        public ConsoleUI(IPreparer.Factory preparerFactory, IPalette.Factory paletteFactory, IPainter.Factory painterFactory)
+        public ConsoleUI(IPreparer.Factory preparerFactory, IPalette.Factory paletteFactory,
+            IPainter.Factory painterFactory)
         {
             this.preparerFactory = preparerFactory;
             this.paletteFactory = paletteFactory;
@@ -20,62 +21,29 @@ namespace WordCloudGenerator
 
         public void Run()
         {
-            var prepared = PrepareWords();
+            var text = AskFilePathAndRead();
+            var (wordsToSkip, wordCount) = AskPreparerSettings();
+            var preparer = preparerFactory(wordsToSkip);
+            var frequencies = PerformAndReport("Обработка текста", () => preparer.CreateWordFreqList(text, wordCount));
 
-            var graphicStrings = GenerateCloud(prepared);
+            var algorithm = AskGenerationAlgorithm();
+            var graphicStrings = PerformAndReport("Генерация облака", () => algorithm(frequencies));
 
-            var palette = EnterPalette();
-            var painter = painterFactory(palette);
-            var img = PerformAndReport("Рисование облака", () => painter.Paint(graphicStrings));
+            var palette = AskPalette();
+            var font = AskFont();
+            var painter = painterFactory(palette, font);
+
+            var img = Result.RepeatUntilOk(() =>
+            {
+                var size = AskSize();
+                return painter.Paint(graphicStrings, size);
+            }, Console.WriteLine).Value;
 
             var pathToSave = AskUntilCorrect("путь по которому сохранить изображение", Saver.IsPathCorrect,
                 "путь неверный");
             Saver.SaveImage(img, pathToSave);
 
             Console.WriteLine($"Изображение сохранено в {pathToSave}");
-        }
-
-        private IPalette EnterPalette()
-        {
-            var colorConverter = new ColorConverter();
-            
-            // ReSharper disable once PossibleNullReferenceException
-            // cant be null because of CanConvertFrom
-            var bgColor = (Color) colorConverter.ConvertFromString(AskUntilCorrect("цвет фона",
-                colorConverter.CanConvertFrom, "такого цвета не существует").Trim());
-
-            var colorsStr =
-                RepeatUntil(() => AskUntilCorrect("цвета палитры (пустая строка чтобы прекратить)",
-                    colorConverter.CanConvertFrom,
-                    "такого цвета не существует"), string.IsNullOrEmpty);
-            var mainColors = colorsStr.Select(colorConverter.ConvertFromString).Cast<Color>();
-
-            var palette = paletteFactory(mainColors, bgColor);
-            return palette;
-        }
-
-        private IEnumerable<GraphicString> GenerateCloud(IEnumerable<WordFrequency> prepared)
-        {
-            var algoChoice = AskUntilCorrect(
-                "какой алгоритм использовать - экспоненциальный или пропорциональный? (1/2)",
-                str => str == "1" || str == "2", "нужно выбрать 1 или 2");
-            var algorithm = AlgorithmFabric.Create((AlgorithmType) int.Parse(algoChoice));
-            return algorithm(prepared);
-        }
-
-        private IEnumerable<WordFrequency> PrepareWords()
-        {
-            var pathToText = Ask("полный путь к файлу с текстом");
-            var text = Reader.ReadFile(pathToText);
-            var wordsToSkip = RepeatUntil(
-                () => Ask("Введите слово которое нужно пропускать (пустая строка, чтобы закончить)"),
-                string.IsNullOrEmpty);
-
-            var preparer = preparerFactory(wordsToSkip);
-            var wordCount = int.Parse(AskUntilCorrect("сколько слов должно быть в облаке",
-                str => int.TryParse(str, out _), "это не является числом"));
-            
-            return PerformAndReport("Обработка текста", () => preparer.CreateWordFreqList(text, wordCount));
         }
 
         private string Ask(string what)
@@ -121,6 +89,81 @@ namespace WordCloudGenerator
         private void ClearLine()
         {
             Console.Write('\r' + new string(' ', Console.BufferWidth));
+        }
+
+        private string AskFilePathAndRead()
+        {
+            var readingResult = Result.RepeatUntilOk(() =>
+            {
+                var pathToText = Ask("полный путь к файлу с текстом");
+                return Reader.ReadFile(pathToText);
+            }, Console.WriteLine);
+
+            return readingResult.Value;
+        }
+
+        private (IEnumerable<string> wordsToSkip, int wordCount) AskPreparerSettings()
+        {
+            var wordsToSkip = RepeatUntil(
+                () => Ask("Введите слово которое нужно пропускать (пустая строка, чтобы закончить)"),
+                string.IsNullOrEmpty);
+
+            var wordCount = Result.RepeatUntilOk(() => int.Parse(Ask("колличество слов в облаке")),
+                s => Console.WriteLine("Невозможно привести к целому числу"));
+            return (wordsToSkip, wordCount.Value);
+        }
+
+        private Func<IEnumerable<WordFrequency>, IEnumerable<GraphicString>> AskGenerationAlgorithm()
+        {
+            var algoChoice = AskUntilCorrect(
+                "какой алгоритм использовать - экспоненциальный или пропорциональный? (1/2)",
+                str => str == "1" || str == "2", "нужно выбрать 1 или 2");
+            return AlgorithmFabric.Create((AlgorithmType) int.Parse(algoChoice));
+        }
+
+        private FontFamily AskFont()
+        {
+            var fonts = FontFamily.Families;
+            var fontResult = Result.RepeatUntilOk(() =>
+            {
+                var fontName = Ask("каким шрифтом рисовать").ToLower();
+                return fonts.First(font => fontName.ToLower() == fontName);
+            }, err => Console.WriteLine("Такого шрифта не существует"));
+
+            return fontResult.Value;
+        }
+
+        private IPalette AskPalette()
+        {
+            var colorConverter = new ColorConverter();
+
+            var bgColor = colorConverter.ConvertFromString(AskUntilCorrect("цвет фона",
+                colorConverter.CanConvertFrom, "такого цвета не существует"));
+
+            var colorsStr =
+                RepeatUntil(() => AskUntilCorrect("цвета палитры (пустая строка чтобы прекратить)",
+                    colorConverter.CanConvertFrom,
+                    "такого цвета не существует"), string.IsNullOrEmpty);
+            var mainColors = colorsStr.Select(colorConverter.ConvertFromString).Cast<Color>();
+
+            var palette = paletteFactory(mainColors, (Color) bgColor);
+            return palette;
+        }
+
+        private Size AskSize()
+        {
+            var way = AskUntilCorrect("задать размер самому, или посчитать автоматически (1/2)",
+                s => s == "1" || s == "2", "нужно ввести 1 или 2");
+            if (way == "2")
+                return Size.Empty;
+
+            var width = AskUntilCorrect("ширину", s => int.TryParse(s, out var n) && n > 0,
+                "Должна быть числом, больше 0");
+
+            var height = AskUntilCorrect("высоту", s => int.TryParse(s, out var n) && n > 0,
+                "Должна быть числом, больше 0");
+
+            return new Size(int.Parse(width), int.Parse(height));
         }
     }
 }
