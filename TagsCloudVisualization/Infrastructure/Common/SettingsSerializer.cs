d88970dc4a71c743eff07c4b9baa5c603488d1e2
AppleePie <64743297+AppleePie@users.noreply.github.com>
2020-12-14 02:11:45 +0500

    Added message to user about files missing with railway

--- TagsCloudVisualization/Infrastructure/Common/SettingsSerializer.cs
+++ TagsCloudVisualization/Infrastructure/Common/SettingsSerializer.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using Newtonsoft.Json;
+using TagsCloudCreating.Infrastructure;
 
 namespace TagsCloudVisualization.Infrastructure.Common
 {
@@ -10,9 +11,21 @@ namespace TagsCloudVisualization.Infrastructure.Common
 
         public static SettingsManager Deserialize()
         {
-            SettingsManager = JsonConvert.DeserializeObject<SettingsManager>(File.ReadAllText(PathToSettings));
+            var settingsManager = ReadAllText(PathToSettings)
+                .Then(JsonConvert.DeserializeObject<SettingsManager>)
+                .OnFail(_ => ShowMessage());
+
+            SettingsManager = settingsManager.IsSuccess ? settingsManager.GetValueOrThrow() : new SettingsManager();
             return SettingsManager;
         }
+
+        private static void ShowMessage() =>
+            InformationMessageHelper.ShowExceptionMessage($"{PathToSettings} not found! Default settings applied.");
+
+        private static Result<string> ReadAllText(string path) =>
+            File.Exists(path)
+                ? File.ReadAllText(PathToSettings)
+                : Result.Fail<string>("File is not found!");
 
         public static void Serialize() =>
             File.WriteAllText(PathToSettings, JsonConvert.SerializeObject(SettingsManager));
--- TagsCloudVisualization/Infrastructure/FileWordsReader.cs
+++ TagsCloudVisualization/Infrastructure/FileWordsReader.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text.RegularExpressions;
+using TagsCloudCreating.Infrastructure;
 using TagsCloudVisualization.Contracts;
 
 namespace TagsCloudVisualization.Infrastructure
 {
     public class FileWordsReader : IWordsReader
     {
-        public IEnumerable<string> GetAllData(string fileName) => Regex.Split(File.ReadAllText(fileName), @"\W+");
+        public Result<IEnumerable<string>> GetAllData(string fileName) =>
+            fileName.AsResult()
+                .Then(File.ReadAllText)
+                .Then(words => Regex.Split(words, @"\W+").AsEnumerable())
+                .ReplaceError(err => "File is not exist");
     }
 }
