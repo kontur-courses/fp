039dd0567f1a159aead58654767502f8a05c241d
BandLComplete <boroviki2000@mail.ru>
2020-12-13 14:39:20 +0500

    Убрал сохранение точек.

--- TagsCloud/PointsLayouts/SpiralPoints.cs
+++ TagsCloud/PointsLayouts/SpiralPoints.cs
@@ -6,42 +6,18 @@ namespace TagsCloud.PointsLayouts
 {
     public class SpiralPoints : IPointsLayout
     {
-        private readonly HashSet<Point> generatedPoints = new HashSet<Point>{new Point()};
-        private int radius = 1;
-        private int pointsInCircleCount = 4;
-        
         public IEnumerable<Point> GetPoints()
         {
-            foreach (var point in generatedPoints)
-                yield return point;
-            
-            while (true)
-            {
-                var circlePoints = GetCirclePoints();
-                foreach (var point in circlePoints)
-                    generatedPoints.Add(point);
-                radius += 8;
-                pointsInCircleCount = radius;
+            yield return new Point();
 
-                foreach (var point in circlePoints)
-                    yield return point;
-            }
-        }
-
-        private HashSet<Point> GetCirclePoints()
-        {
-            var circlePoints = new HashSet<Point>();
-            
+            for (int radius = 1, pointsInCircleCount = 4;; radius+=8, pointsInCircleCount=radius)
             for (var i = 0; i < pointsInCircleCount; ++i)
             {
-                var x = (int)(Math.Cos(2 * Math.PI * i / pointsInCircleCount) * radius + 0.5);
-                var y = (int)(Math.Sin(2 * Math.PI * i / pointsInCircleCount) * radius + 0.5);
+                var x = (int) (Math.Cos(2 * Math.PI * i / pointsInCircleCount) * radius + 0.5);
+                var y = (int) (Math.Sin(2 * Math.PI * i / pointsInCircleCount) * radius + 0.5);
 
-                var point = new Point(x, y);
-                circlePoints.Add(point);
+                yield return new Point(x, y);
             }
-
-            return circlePoints;
         }
     }
 }
--- TagsCloud/PointsLayouts/SquarePoints.cs
+++ TagsCloud/PointsLayouts/SquarePoints.cs
@@ -6,45 +6,25 @@ namespace TagsCloud.PointsLayouts
 {
     public class SquarePoints : IPointsLayout
     {
-        private readonly HashSet<Point> generatedPoints = new HashSet<Point>{new Point()};
-        private int radius = 1;
-        
         public IEnumerable<Point> GetPoints()
         {
-            foreach (var point in generatedPoints)
-                yield return point;
-            
-            while (true)
-            {
-                var squarePoints = GetSquarePoints();
-                foreach (var point in squarePoints)
-                    generatedPoints.Add(point);
-                radius+=10;
+            yield return new Point();
 
-                foreach (var point in squarePoints)
-                    yield return point;
-            }
-        }
-
-        private HashSet<Point> GetSquarePoints()
-        {
-            var squarePoints = new HashSet<Point>();
-
+            for(var radius = 1;;radius+=10)
             for (var x = -radius; x <= radius; x++)
             {
                 if (Math.Abs(x) != radius)
                 {
-                    squarePoints.Add(new Point(x, radius));
-                    squarePoints.Add(new Point(x, -radius));
+                    yield return new Point(x, radius);
+                    yield return new Point(x, -radius);
                     continue;
                 }
+
                 for (var y = -radius; y <= radius; y++)
                 {
-                    squarePoints.Add(new Point(x, y));
+                    yield return new Point(x, y);
                 }
             }
-
-            return squarePoints;
         }
     }
 }
