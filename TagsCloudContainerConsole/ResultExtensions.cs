73ba8c990ab501ad78b066b9edfa52133e4dad4c
Denis Bautin <denis020301@mail.ru>
2020-12-13 05:02:28 +0500

    Create ResultExtensions class and tests for it

---
+++ TagsCloudContainerConsole/ResultExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ResultOf;
+
+namespace TagsCloudContainer
+{
+    public static class ResultExtensions
+    {
+        public static Result<T> Validate<T>(
+            this Result<T> result, Func<T, bool> isValid, string errorMessage)
+        {
+            if (result.IsSuccess && !isValid(result.Value))
+                return Result.Fail<T>(errorMessage);
+            
+            return result;
+        }
+    }
+}
---
+++ TagsCloudContainerTests/ResultExtensionsTest.cs
@@ -0,0 +1,38 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using ResultOf;
+using TagsCloudContainer;
+
+namespace TagsCloudContainerTests
+{
+    [TestFixture]
+    public class ResultExtensionsTest
+    {
+        [Test]
+        public void Validate_IfCalledFromFailResult_ReturnThisResult()
+        {
+            var failResult = Result.Fail<int>("error message");
+
+            failResult.Validate(num => num % 2 == 0, "other error").Should()
+                .Be(failResult);
+        }
+
+        [Test]
+        public void Validate_IfValueIsNotValid_ReturnFailResultWithGivenError()
+        {
+            var resultObj = Result.Ok(5);
+
+            resultObj.Validate(num => num % 2 == 0, "num error").Error
+                .Should().Be("num error");
+        }
+
+        [Test]
+        public void Validate_IfValueIsValid_ReturnThisResult()
+        {
+            var resultObj = Result.Ok(4);
+
+            resultObj.Validate(num => num % 2 == 0, "num error")
+                .Should().Be(resultObj);
+        }
+    }
+}
