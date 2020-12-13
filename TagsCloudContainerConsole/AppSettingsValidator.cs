385c0bd4f9f5bf358884fe86d825bd9ed6546be4
Denis Bautin <denis020301@mail.ru>
2020-12-13 05:02:42 +0500

    Create AppSettingsValidator class and tests for it

---
+++ TagsCloudContainerConsole/AppSettingsValidator.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace TagsCloudContainer
+{
+    public class AppSettingsValidator
+    {
+        public AppSettings Settings { get; }
+
+        public AppSettingsValidator(AppSettings settings)
+        {
+            Settings = settings;
+        }
+
+        public AppSettingsValidator ValidateTextFilePath(string[] supportedTextFormats)
+        {
+            if (!File.Exists(Settings.PathToFile))
+                throw new ArgumentException($"The specified file does not exist: {Settings.PathToFile}");
+
+            CheckForItem(Path.GetExtension(Settings.PathToFile), supportedTextFormats,
+                $"Format of this file isn't supported: {Settings.PathToFile}\n" +
+                $"Supported formats: {string.Join(", ", supportedTextFormats)}");
+
+            return this;
+        }
+
+        public AppSettingsValidator ValidatePartsOfSpeech()
+        {
+            var partsOfSpeech = Enum.GetNames(typeof(PartOfSpeech));
+            foreach (var name in Settings.ExcludedPartsOfSpeechNames)
+                CheckForItemIgnoreCase(name, partsOfSpeech, $"Incorrect name of part of speech: {name}");
+
+            return this;
+        }
+
+        public AppSettingsValidator ValidateColors()
+        {
+            var colors = Enum.GetNames(typeof(KnownColor));
+
+            CheckForItemIgnoreCase(Settings.BackgroundColorName, colors,
+                $"Incorrect background color: {Settings.BackgroundColorName}");
+
+            CheckForItemIgnoreCase(Settings.FontColorName, colors,
+                $"Incorrect font color: {Settings.FontColorName}");
+
+            return this;
+        }
+
+        public AppSettingsValidator ValidateFont()
+        {
+            var fontNames = FontFamily.Families.Select(family => family.Name);
+            CheckForItemIgnoreCase(Settings.FontName, fontNames, $"Incorrect font: {Settings.FontName}");
+
+            return this;
+        }
+
+        public AppSettingsValidator ValidateImagePath(string[] validExtensions)
+        {
+            var ext = Path.GetExtension(Settings.ImagePath);
+            CheckForItem(ext, validExtensions, $"Incorrect image format: {ext}");
+
+            return this;
+        }
+
+        private static void CheckForItem(string item, IEnumerable<string> allItems, string errorMessage)
+        {
+            if (!allItems.Contains(item))
+                throw new ArgumentException(errorMessage);
+        }
+        
+        private static void CheckForItemIgnoreCase(string item, IEnumerable<string> allItems, string errorMessage)
+        {
+            if (!allItems.Contains(item, StringComparer.OrdinalIgnoreCase))
+                throw new ArgumentException(errorMessage);
+        }
+    }
+}
---
+++ TagsCloudContainerTests/AppSettingsValidatorTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+using TagsCloudContainer;
+
+namespace TagsCloudContainerTests
+{
+    [TestFixture]
+    public class AppSettingsValidatorTests
+    {
+        private AppSettings settings;
+        private AppSettingsValidator validator;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            settings = new AppSettings();
+            validator = new AppSettingsValidator(settings);
+        }
+
+        [Test]
+        public void ValidateTextFilePath_IfFileDoesNotExist_ThrowArgumentException()
+        {
+            settings.PathToFile = "blabla.txt";
+            Action act = () => validator.ValidateTextFilePath(new[] {".txt"});
+
+            act.Should().Throw<ArgumentException>();
+        }
+        
+        [Test]
+        public void ValidateTextFile_IfFileFormatIsNotSupported_ThrowArgumentException()
+        {
+            settings.PathToFile = "../../../TestFiles/test.docx";
+            Action act = () => validator.ValidateTextFilePath(new[] {".txt"});
+
+            act.Should().Throw<ArgumentException>();
+        }
+        
+        [Test]
+        public void ValidatePartsOfSpeech_IfIncorrectPartsOfSpeechName_ThrowArgumentException()
+        {
+            settings.ExcludedPartsOfSpeechNames = new[] { "blabla" };
+            Action act = () => validator.ValidatePartsOfSpeech();
+
+            act.Should().Throw<ArgumentException>();
+        }
+        
+        [Test]
+        public void ValidateColors_IfIncorrectFontColorName_ThrowArgumentException()
+        {
+            settings.FontColorName = "i'm not a color";
+            settings.BackgroundColorName = "White";
+            Action act = () => validator.ValidateColors();
+
+            act.Should().Throw<ArgumentException>();
+        }
+        
+        [Test]
+        public void ValidateColors_IfIncorrectBackgroundColorName_ThrowArgumentException()
+        {
+            settings.FontColorName = "Black";
+            settings.BackgroundColorName = "i'm not a color";
+            Action act = () => validator.ValidateColors();
+
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void ValidateFont_IfIncorrectFontName_ThrowArgumentException()
+        {
+            settings.FontName = "abcdef";
+            Action act = () => validator.ValidateFont();
+
+            act.Should().Throw<ArgumentException>();
+        }
+
+        [Test]
+        public void ValidateImagePath_IfFormatIsNotSupported_ThrowArgumentException()
+        {
+            settings.ImagePath = "image.gpn";
+            Action act = () => validator.ValidateImagePath(new[] {".png"});
+
+            act.Should().Throw<ArgumentException>();
+        }
+    }
+}
