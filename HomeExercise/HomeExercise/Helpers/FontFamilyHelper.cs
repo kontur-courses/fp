eb15a9559c51997c0fd52ae6e77bbf900942fe78
FoolLikeYou <Lambda2712@gmail.com>
2020-12-15 01:57:26 +0500

    added new helper

---
+++ HomeExercise/HomeExercise/Helpers/FontFamilyHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Drawing;
+
+namespace HomeExercise.Helpers
+{
+    public static class FontFamilyHelper
+    {
+        public static FontFamily GetFontFamily(string stringRepresentations)
+        {
+            return new FontFamily(stringRepresentations);
+        }
+    }
+}
