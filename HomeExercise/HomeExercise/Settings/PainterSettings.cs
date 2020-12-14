45e509c36d35c08860d1cf63f2eec52ce914dadb
FoolLikeYou <Lambda2712@gmail.com>
2020-12-15 02:06:48 +0500

    updated word

--- HomeExercise/HomeExercise/Settings/PainterSettings.cs
+++ HomeExercise/HomeExercise/Settings/PainterSettings.cs
@@ -1,20 +1,18 @@
 ﻿using System.Drawing;
 using System.Drawing.Imaging;
 
-namespace HomeExercise.settings
+namespace HomeExercise.Settings
 {
     public class PainterSettings
     {
         public Color Color { get; }
-        public int Width { get; }
-        public int Height { get; }
+        public Size Size { get; }
         public string FileName { get; }
         public ImageFormat Format { get; }
         
-        public PainterSettings(int width, int height, string fileName, ImageFormat format, Color color)
+        public PainterSettings(Size size, string fileName, ImageFormat format, Color color)
         {
-            Width = width;
-            Height = height;
+            Size = size;
             FileName = fileName;
             Format = format;
             Color = color;
