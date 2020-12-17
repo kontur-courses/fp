bec4d7dc09e9aa6330a0279926d87bd4378e7439
MaxCross <dan25001@gmail.com>
2020-12-17 23:07:22 +0500

    Тесты на Result

---
+++ TagsCloudContainer/Tests/TagsCloudContainer_Result_Should.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TagsCloudContainer
+{
+    public class TagsCloudContainer_Result_Should
+    {
+        private TagsCloudContainer container;
+        private WordsRendererToImageDebug renderer;
+        private string basicWords = "some basic Words toTest";
+
+        [Test]
+        public void Fail_WhenFileWithWordsDoesNotExist()
+        {
+            var fileName = "Some_Not_Existing_File..."; 
+            File.Exists(fileName).Should().BeFalse();
+            
+            container.AddFromFile(fileName);
+            var result = container.Render();
+            result.IsSuccess.Should().BeFalse();
+            result.Error.Should().StartWith("Не удаётся прочитать слова из источника. ");
+            result.Error.Should().Contain($"Не удаётся прочитать файл {fileName}");
+        }
+
+        [Test]
+        public void Fail_WhenPreprocessingFunctionThrowsSomething()
+        {
+            var result = container
+                .AddFromText(basicWords)
+                .Preprocessing(word => throw new Exception($"!hello, {word}"))
+                .Render();
+            
+            result.IsSuccess.Should().BeFalse();
+            result.Error.Should().StartWith("Не удалось преобразовать слово. ");
+            result.Error.Should().Contain("!hello, ");
+        }
+        
+        [SetUp]
+        public void SetUp()
+        {
+            renderer = new WordsRendererToImageDebug();
+            container = new TagsCloudContainer()
+                .Rendering(renderer);
+        }
+
+        private static TextBuilder GetRandomText(params string[][] words) => new TextBuilder(words);
+        private string[] regularWords => TextBuilder.regularWords;
+        private string[] wordsToExclude => TextBuilder.wordsToExclude;
+    }
+}
