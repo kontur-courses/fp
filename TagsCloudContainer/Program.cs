95af514cc5378df29d8fc6e0c2f1234c33b786f7
FreeTrally <serega2012111@gmail.com>
2021-12-15 00:11:41 +0500

    Other files

---
+++ TagsCloudContainer/Program.cs
@@ -0,0 +1,16 @@
+﻿using LightInject;
+using TagsCloudContainer.Infrastructure;
+using TagsCloudContainer.Interfaces;
+
+namespace TagsCloudContainer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var container = ContainerProvider.GetContainer();
+            var ui = container.GetInstance<IUI>();
+            ui.Run();
+        }
+    }
+}
