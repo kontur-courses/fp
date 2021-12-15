9d6f867cb43e8da684d8b30994cf21abf2ce4025
Aripov Sergey <aripovsergey3@gmail.com>
2021-12-15 13:12:09 +0500

    Добавил Result

---
+++ Results/Result.cs
@@ -0,0 +1,36 @@
+﻿namespace ResultOf;
+
+public struct Result
+{
+    internal Result(string? error)
+    {
+        Error = error;
+    }
+
+    public string? Error { get; }
+    public bool IsSuccess => Error == null;
+}
+
+public struct Result<T>
+{
+    internal Result(string? error, T? value = default)
+    {
+        Error = error;
+        Value = value;
+    }
+
+    public static implicit operator Result<T>(T v)
+    {
+        return new(null, v);
+    }
+
+    public string? Error { get; }
+    internal T? Value { get; }
+    public T GetValueOrThrow()
+    {
+        if (IsSuccess)
+            return Value!;
+        throw new InvalidOperationException($"No value. Only Error {Error}");
+    }
+    public bool IsSuccess => Error == null;
+}
