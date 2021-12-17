0393f2c580f55fef9f8e7bf4beb61bbf85177bc6
Aripov Sergey <aripovsergey3@gmail.com>
2021-12-17 23:30:21 +0500

    Тесты

---
+++ ContainerTests/ContainerTests.cs
@@ -0,0 +1,137 @@
+using FluentAssertions;
+using NUnit.Framework;
+using ResultOf;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using TagsCloudContainer;
+using TagsCloudContainer.Abstractions;
+using TagsCloudContainer.Defaults;
+using TagsCloudContainer.Registrations;
+
+namespace ContainerTests;
+
+public class ContainerTests
+{
+
+    [Test]
+    public void CanChangeUsableImplementations()
+    {
+        var args = new[] { "--assemblies", Assembly.GetExecutingAssembly().Location, "--implement-with", $"{nameof(IRunner)} {nameof(ThrowingRunner)}" };
+        var result = InitializationHelper.RunWithArgs(args);
+
+        result.IsSuccess.Should().BeFalse();
+        result.Error.Should().Contain(ThrowingRunner.ExceptionText);
+    }
+
+    [Test]
+    public void ShouldReturnOkResult_WhenAtLeastRequiredSettingsProvided()
+    {
+        var result = InitializationHelper.RunWithArgs(new[] { "--string", "тэгер тэг тэги" });
+
+        result.IsSuccess.Should().BeTrue();
+    }
+
+    [TestCaseSource(nameof(IncorrectArgsSource))]
+    public void ShouldReturnFailedResult_WhenBothDefaultInputProvided(IncorrectArg arg)
+    {
+        var result = InitializationHelper.RunWithArgs(arg.Args);
+
+        result.IsSuccess.Should().BeFalse();
+        result.Error.Should().Contain("Failed to run").And.Contain(arg.Why);
+    }
+
+    [Test]
+    public void ShouldReturnFailedResult_WhenNoRequiredSettingsProvided()
+    {
+        var result = InitializationHelper.RunWithArgs(Array.Empty<string>());
+
+        result.IsSuccess.Should().BeFalse();
+        result.Error.Should().Contain("One of the required arguments");
+    }
+
+    [TestCase("Abc", nameof(DefaultRunner))]
+    [TestCase(nameof(IRunner), "Abc")]
+    public void ShouldReturnFailedResult_WhenInvalidImplementationArgs(string service, string implementation)
+    {
+        var result = InitializationHelper.RunWithArgs(new[] { "--implement-with", $"{service} {implementation}" });
+
+        result.IsSuccess.Should().BeFalse();
+        result.Error.Should().Contain("Could not find type with name");
+    }
+
+    [Test]
+    public void ShouldReturnFailedResult_WhenUnknownAssemblyIsProvided()
+    {
+        var result = InitializationHelper.RunWithArgs(new[] { "--assemblies", $"abc" });
+
+        result.IsSuccess.Should().BeFalse();
+        result.Error.Should().Contain("Could not load assembly");
+    }
+
+    [Test]
+    public void ServiceIndex_Should_CorrectlyIndexAvailableServicesAndImplementationsFromAssemblies()
+    {
+        var serviceIndex = new ServiceIndex();
+
+        serviceIndex.AddAssemblyTypes(Assembly.GetExecutingAssembly());
+        var implementation = serviceIndex.GetImplementation(nameof(TotallyLegitimateService));
+        var service = serviceIndex.GetService(nameof(ITotallyLegitimateService));
+
+        implementation.IsSuccess.Should().BeTrue();
+        implementation.GetValueOrThrow().Implementation.Should().Be(typeof(TotallyLegitimateService));
+        service.IsSuccess.Should().BeTrue();
+        service.GetValueOrThrow().Should().Be(typeof(ITotallyLegitimateService));
+        implementation.GetValueOrThrow().ImplementedServices.Should().BeEquivalentTo(new[] { service.GetValueOrThrow() });
+    }
+
+    [Test]
+    public void ServiceIndex_ShouldReturnFailedResults_WhenRequestedUnknown()
+    {
+        var serviceIndex = new ServiceIndex();
+
+        var implementation = serviceIndex.GetImplementation(nameof(TotallyLegitimateService));
+        var service = serviceIndex.GetService(nameof(ITotallyLegitimateService));
+
+        implementation.IsSuccess.Should().BeFalse();
+        implementation.Error.Should().Contain($"Could not find type with name '{nameof(TotallyLegitimateService)}'.");
+        service.IsSuccess.Should().BeFalse();
+        service.Error.Should().Contain($"Could not find type with name '{nameof(ITotallyLegitimateService)}'.");
+    }
+
+    private static IEnumerable<IncorrectArg> IncorrectArgsSource()
+    {
+        yield return new(new[] { "--files", "abc" }, "Could not find file");
+        yield return new(new[] { "--string", "тэгер тэг тэги", "--files", "abc" }, "Can't use both file and string input providers");
+        yield return new(new[] { "--center", "abc" }, "String abc was in incorrect format, should be two ints separated by ','");
+        yield return new(new[] { "--image-format", "abc" }, "Could not convert string `abc' to type ImageFormatType");
+        yield return new(new[] { "--add-parts", "abc" }, "Requested value 'abc' was not found.");
+        yield return new(new[] { "--min-size", "abc" }, "Could not convert string `abc' to type Double");
+        yield return new(new[] { "--color", "abc" }, "Could not convert string `abc' to type Color");
+        yield return new(new[] { "--height", "abc" }, "Could not convert string `abc' to type Int32");
+        yield return new(new[] { "--width", "abc" }, "Could not convert string `abc' to type Int32");
+        yield return new(new[] { "--smoothing-mode", "abc" }, "Could not convert string `abc' to type SmoothingMode");
+    }
+
+    public record IncorrectArg(string[] Args, string Why);
+
+    private class ThrowingRunner : IRunner
+    {
+        public const string ExceptionText = "tried to use throwing runner";
+        public Result Run(params string[] args)
+        {
+            return Result.Fail(ExceptionText);
+        }
+    }
+
+    private interface ITotallyLegitimateService : IService
+    {
+
+    }
+
+    private class TotallyLegitimateService : ITotallyLegitimateService
+    {
+
+    }
+}
+
---
+++ ContainerTests/DefaultsTests.cs
@@ -0,0 +1,137 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.IO;
+using System.Linq;
+using TagsCloudContainer.Abstractions;
+using TagsCloudContainer.Defaults;
+using TagsCloudContainer.Defaults.MyStem;
+using TagsCloudContainer.Defaults.SettingsProviders;
+
+namespace ContainerTests;
+
+public class DefaultsTests
+{
+    private static TSetting ParseSettings<TSetting>(TSetting settingObj, params string[] args)
+        where TSetting : ICliSettingsProvider
+    {
+        settingObj.GetCliOptions().Parse(args);
+        return settingObj;
+    }
+
+    [Test]
+    public void StringReader_Should_CorrectlyReadText()
+    {
+        var text = $"abcd{Environment.NewLine}efg{Environment.NewLine}hkl";
+        var expectedResult = text.Split(Environment.NewLine, StringSplitOptions.TrimEntries);
+        var inputSettings = ParseSettings(new InputSettings(), "--string", text);
+        var reader = new SimpleStringReader(inputSettings);
+
+        var actualResult = reader.ReadLines().ToArray();
+
+        actualResult.Should().BeEquivalentTo(expectedResult);
+    }
+
+    [Test]
+    public void FileReader_Should_CorrectlyReadText()
+    {
+        var text = $"abcd{Environment.NewLine}efg{Environment.NewLine}hkl";
+        var path = Path.GetTempFileName();
+        File.WriteAllText(path, text);
+
+        var expectedResult = text.Split(Environment.NewLine, StringSplitOptions.TrimEntries);
+        var inputSettings = ParseSettings(new InputSettings(), "--files", path);
+        var reader = new FileReader(inputSettings);
+
+        var actualResult = reader.ReadLines().ToArray();
+
+        actualResult.Should().BeEquivalentTo(expectedResult);
+    }
+
+    [Test]
+    public void TextAnalyzer_Should_CorrectlyCollectTextStats()
+    {
+        var text = new[] { "abcd", "efg", "hkl" };
+        var fakeStats = GetTextStats(text);
+
+        var fakeReader = A.Fake<ITextReader>();
+        A.CallTo(() => fakeReader.ReadLines()).Returns(text);
+
+        var textAnalyzer = new AnalyzerWrapper(fakeReader, Array.Empty<IWordNormalizer>(), Array.Empty<IWordFilter>(), new[] { ' ', ',', '.' });
+
+        var actualResult = textAnalyzer.AnalyzeText();
+
+        actualResult.Statistics.Should().BeEquivalentTo(fakeStats.Statistics);
+        actualResult.TotalWordCount.Should().Be(fakeStats.TotalWordCount);
+    }
+
+    private static ITextStats GetTextStats(string[] text)
+    {
+        var fakeStats = A.Fake<ITextStats>();
+        A.CallTo(() => fakeStats.Statistics).Returns(text.GroupBy(x => x).ToDictionary(x => x.Key, x => x.Count()));
+        A.CallTo(() => fakeStats.TotalWordCount).Returns(text.Length);
+        return fakeStats;
+    }
+
+    [Test]
+    public void StemNormilizer_Should_CorrectlyTakeStemFromWord()
+    {
+        var text = new[] { "кот", "коты", "котов" };
+        var expectedReuslt = new[] { "кот", "кот", "кот" };
+
+        var myStem = new MyStem();
+
+        var stemNormalizer = new StemNormalizer(myStem);
+        var actualResult = text.Select(stemNormalizer.Normalize).Select(x => x.GetValueOrThrow()).ToArray();
+
+        actualResult.Should().BeEquivalentTo(expectedReuslt);
+    }
+
+    [Test]
+    public void SpeechPartFilter_Should_CorrectlyFilterOutSpecifiedParts()
+    {
+        var text = new[] { "кот", "коты", "котов" };
+        var expectedReuslt = Array.Empty<string>();
+
+        var myStem = new MyStem();
+        var settings = ParseSettings(new SpeechPartFilterSettings(), "--add-parts", "S");
+        var filter = new SpeechPartFilter(settings, myStem);
+
+        var actualResult = text.Where(filter.IsValid);
+
+        actualResult.Should().BeEquivalentTo(expectedReuslt);
+    }
+
+    [Test]
+    public void TagPacker_Should_CorrectlyAssembleTags()
+    {
+        var text = new[] { "кот", "коты", "котов" };
+
+        var fakeStats = GetTextStats(text);
+
+        var fakeAnalyzer = A.Fake<ITextAnalyzer>();
+        A.CallTo(() => fakeAnalyzer.AnalyzeText()).Returns(fakeStats);
+
+        var fakeTags = fakeStats.Statistics.Select(x =>
+        {
+            var tag = A.Fake<ITag>();
+            A.CallTo(() => tag.Value).Returns(x.Key);
+            A.CallTo(() => tag.RelativeSize).Returns((double)x.Value / fakeStats.TotalWordCount);
+            return tag;
+        });
+
+        var packer = new TagPacker(fakeAnalyzer);
+
+        var actualResult = packer.GetTags();
+
+        actualResult.Should().BeEquivalentTo(fakeTags);
+    }
+
+    private class AnalyzerWrapper : TextAnalyzer
+    {
+        public AnalyzerWrapper(ITextReader textReader, IWordNormalizer[] wordNormalizers, IWordFilter[] wordFilters, char[] wordSeparators) : base(textReader, wordNormalizers, wordFilters, wordSeparators)
+        {
+        }
+    }
+}
