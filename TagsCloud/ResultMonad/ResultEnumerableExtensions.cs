c8ed7270037b462c8a5d50ab644627e019866dfb
gODealOAple <maksim.math@gmail.com>
2021-12-18 15:56:36 +0500

    Add extensions for Result monads

---
+++ TagsCloud/ResultMonad/ResultEnumerableExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace ResultMonad
+{
+    public static class ResultEnumerableExtensions
+    {
+        public static Result<IEnumerable<TInput>> Traverse<TInput>(this IEnumerable<Result<TInput>> collection)
+        {
+            var list = new List<TInput>();
+            foreach (var value in collection)
+            {
+                if (!value.IsSuccess)
+                    return Result.Fail<IEnumerable<TInput>>(value.Error);
+                list.Add(value.Value);
+            }
+
+            return Result.Ok<IEnumerable<TInput>>(list);
+        }
+    }
+}
---
+++ TagsCloud/ResultMonad/ResultNoneExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ResultMonad
+{
+    public static class ResultNoneExtensions
+    {
+        public static Result<None> ToNone<TInput>(this Result<TInput> input) => input.Then(_ => Result.Ok());
+
+        public static Result<TOutput> Then<TOutput>(this Result<None> input, Func<Result<TOutput>> continuation) =>
+            input.Then(_ => continuation());
+
+        public static Result<TOutput> Then<TOutput>(this Result<None> input, Func<TOutput> continuation) =>
+            input.Then(_ => continuation());
+
+        public static Result<None> Validate(this Result<None> input, Func<bool> validate, string err) =>
+            input.Validate(_ => validate(), err);
+
+        public static Result<TOutput> Then<TOutput>(this Result<None> input, TOutput output) =>
+            input.Then(() => output);
+    }
+}
