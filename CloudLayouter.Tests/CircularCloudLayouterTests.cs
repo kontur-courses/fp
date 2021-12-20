b6b50e592defa1843756f12e6759328772407ebf
Aripov Sergey <aripovsergey3@gmail.com>
2021-12-20 12:31:36 +0500

    CircularCloudLayouter тесты(из tdd), поправки namspace'ов

---
+++ CloudLayouter.Tests/CircularCloudLayouterTests.cs
@@ -0,0 +1,211 @@
+﻿using CloudLayouter;
+using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace CloudLayouter.Tests;
+
+[TestFixture]
+public class CircularCloudLayouterTests
+{
+    private CachedCircularLayouter layouter;
+
+    [Test]
+    public void ShouldHaveSetCenter_OnCreation()
+    {
+        var expectedCenter = new Point(10, 11);
+        layouter = new CachedCircularLayouter(expectedCenter);
+        layouter.Center.Should().Be(expectedCenter);
+    }
+
+    [Test]
+    public void ShouldReturnRectangleWithSetSize()
+    {
+        layouter = new CachedCircularLayouter(Point.Empty);
+        var expectedSize = new Size(10, 11);
+
+        var resulRectangle = layouter.PutNextRectangle(expectedSize);
+
+        resulRectangle.Size.Should().Be(expectedSize);
+    }
+
+    [TestCase(0, 2)]
+    [TestCase(0, 0)]
+    [TestCase(2, 0)]
+    public void ShouldThrow_WhenTryingToPutDegenerateRectangle(int width, int height)
+    {
+        layouter = new CachedCircularLayouter(Point.Empty);
+        Action action = () => layouter.PutNextRectangle(new Size(width, height));
+        action.Should().Throw<ArgumentOutOfRangeException>();
+    }
+
+    [TestCase(0, 0)]
+    [TestCase(10, 11)]
+    public void FirstRectangle_ShouldHaveSameCenter(int x, int y)
+    {
+        var expectedCenter = new Point(x, y);
+        layouter = new CachedCircularLayouter(expectedCenter);
+
+        var resultRectangle = layouter.PutNextRectangle(new Size(10, 11));
+        var resultCenter = resultRectangle.GetCenter();
+
+        resultCenter.Should().Be(expectedCenter);
+    }
+
+    [Test]
+    public void SecondRectangle_ShouldBePlacedNextToFirst()
+    {
+        layouter = new CachedCircularLayouter(Point.Empty);
+
+        var firstRectangle = layouter.PutNextRectangle(new Size(10, 11));
+        var secondRectangle = layouter.PutNextRectangle(new Size(11, 10));
+
+        secondRectangle.Should()
+            .Match<Rectangle>(second => IsNextToEachOther(firstRectangle, second));
+    }
+
+    private static bool IsNextToEachOther(Rectangle first, Rectangle second)
+    {
+        return first.Bottom == second.Top
+               || first.Top == second.Bottom
+               || first.Left == second.Right
+               || first.Right == second.Left;
+    }
+
+    [Test]
+    public void SecondRectangle_ShouldBePlacedAsCloseToCenterAsPossible()
+    {
+        var center = Point.Empty;
+        layouter = new CachedCircularLayouter(center);
+        var firstSize = new Size(10, 11);
+        var secondSize = new Size(10, 11);
+
+        var maximumExpectedDistance = GetClosestDistance(firstSize, secondSize);
+
+        layouter.PutNextRectangle(firstSize);
+        var secondRectangle = layouter.PutNextRectangle(secondSize);
+        var actualDistance = center.DistanceTo(secondRectangle.GetCenter());
+
+        actualDistance.Should().BeLessOrEqualTo(maximumExpectedDistance + 1);
+    }
+
+    [Test]
+    public void ThirdRectangle_ShouldBePlacedAsCloseToCenterAsPossible()
+    {
+        var center = Point.Empty;
+        layouter = new CachedCircularLayouter(center);
+        var firstSize = new Size(100, 11);
+        var secondSize = new Size(10, 11);
+        var thirdSize = new Size(20, 13);
+
+        var maximumExpectedDistance = GetClosestDistance(firstSize, thirdSize);
+
+
+        layouter.PutNextRectangle(firstSize);
+        layouter.PutNextRectangle(secondSize);
+        var thirdRectangle = layouter.PutNextRectangle(thirdSize);
+        var actualDistance = center.DistanceTo(thirdRectangle.GetCenter());
+
+        actualDistance.Should().BeLessOrEqualTo(maximumExpectedDistance + 1);
+    }
+
+    private static double GetClosestDistance(Size origin, Size toBePlaced)
+    {
+        bool isHorizontal;
+        int minimalSide;
+        if (origin.Height > origin.Width)
+        {
+            isHorizontal = true;
+            minimalSide = origin.Width;
+        }
+        else
+        {
+            isHorizontal = false;
+            minimalSide = origin.Height;
+        }
+        return Math.Ceiling(minimalSide / 2d + (isHorizontal ? toBePlaced.Width : toBePlaced.Height) / 2d);
+    }
+
+    [TestCase(3)]
+    [TestCase(300)]
+    public void EveryRectangle_ShouldNotIntersectWithEachOther(int count)
+    {
+        layouter = new CachedCircularLayouter(Point.Empty);
+        var size = new Size(10, 11);
+
+        for (var i = 0; i < count; i++)
+        {
+            layouter.PutNextRectangle(size);
+        }
+
+        var resultRectangles = layouter.Rectangles.Select((rectangle, i) => (rectangle, i));
+
+        resultRectangles.Should()
+            .OnlyContain(rectangle => resultRectangles
+                .Where(x => x.i != rectangle.i)
+                .All(x => !x.rectangle.IntersectsWith(rectangle.rectangle)),
+            "every rectangle should not intersect with others");
+    }
+
+    [TestCase(100, 50)]
+    [TestCase(1000, 50)]
+    public void CloudForm_ShouldBeCloseToCircle(int count, int maxSize)
+    {
+        var center = Point.Empty;
+        layouter = new CachedCircularLayouter(center);
+        var rnd = new Random();
+
+        var radius = 0d;
+        var area = 0;
+        for (var i = 0; i < count; i++)
+        {
+            var size = new Size(rnd.Next(maxSize) + 1, rnd.Next(maxSize) + 1);
+            area += size.GetArea();
+            var rectangle = layouter.PutNextRectangle(size);
+            var maxDistance = rectangle.GetCenter().DistanceTo(center);
+            if (maxDistance > radius)
+                radius = maxDistance;
+        }
+
+        var mainCircleArea = Math.PI * radius * radius;
+        var resultingDensity = area / mainCircleArea;
+
+        resultingDensity.Should().BeGreaterThan(0.7);
+    }
+
+    [TestCase(20)]
+    public void PuttingRectanglesShouldBeFast(int seconds)
+    {
+        layouter = new CachedCircularLayouter(Point.Empty);
+        var size = new Size(10, 11);
+
+        Action action = () =>
+          {
+              for (var i = 0; i < 1300; i++)
+              {
+                  layouter.PutNextRectangle(size);
+              }
+          };
+
+        action.ExecutionTime().Should().BeLessOrEqualTo(new TimeSpan(0, 0, 0, seconds));
+    }
+
+    private class CachedCircularLayouter : CircularCloudLayouter
+    {
+        public IReadOnlyList<Rectangle> Rectangles => rectangles;
+
+        private readonly List<Rectangle> rectangles = new();
+        public CachedCircularLayouter(Point center) : base(center)
+        { }
+
+        public new Rectangle PutNextRectangle(Size size)
+        {
+            var rectangle = base.PutNextRectangle(size);
+            rectangles.Add(rectangle);
+            return rectangle;
+        }
+    }
+}
