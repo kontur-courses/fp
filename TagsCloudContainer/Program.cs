5d4061623e20dce3c1f6531600a16201f1fca8dd
Aripov Sergey <aripovsergey3@gmail.com>
2021-12-20 18:06:21 +0500

    Убрал заглушку настроек

--- TagsCloudContainer/Program.cs
+++ TagsCloudContainer/Program.cs
@@ -3,13 +3,6 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        args = new[] { "--files", "bigbook.txt",
-            "--height", "10000",
-            "--width", "10000",
-            "--center", "5000, 5000",
-            "--word-limit", "3000",
-            "--color", "red",
-            "--implement-with", $"IWordFilter none" };
         InitializationHelper.RunWithArgs(args);
     }
 }
