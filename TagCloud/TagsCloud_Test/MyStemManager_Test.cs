ee6ce0151e5ecbe36f6d2e8f77d0e62ae3bfa4d3
Roman <roman.teckk@gmail.com>
2021-12-22 00:29:59 +0500

    Добавил тесты на корректный result - при отсутствии текстового файла - при ошибке обработки файла в Mystem

---
+++ TagCloud/TagsCloud_Test/MyStemManager_Test.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using FluentAssertions;
+using NUnit.Framework;
+using TagCloud.TextProcessing;
+
+namespace TagsCloud_Test
+{
+    public class MyStemManager_Test
+    {
+        private MyStemManager _myStemManager;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _myStemManager = new MyStemManager();
+        }
+
+        [Test]
+        public void GetLexemesFrom_Failure_WhenMyStemErrorOutNotEmpty_BecauseOfIncorrectEncoding()
+        {
+            var lexemes = _myStemManager.GetLexemesFrom("text_ansi.txt");
+            lexemes.IsSuccess.Should().BeFalse();
+            Console.WriteLine(lexemes.Error);
+        }
+
+        [Test]
+        public void GetLexemesFrom_Success_WhenMyStemWorkedNormally_OnCorrectFile()
+        {
+            var path = Path.Combine(Path.GetTempPath(), "test.txt");
+            using var writer = new StreamWriter(path, false, Encoding.UTF8);
+            writer.WriteLine("Новый тестовый файл в кодировке UTF-8");
+            var lexemes = _myStemManager.GetLexemesFrom(path);
+            lexemes.IsSuccess.Should().BeTrue();
+        }
+    }
+}
---
+++ TagCloud/TagsCloud_Test/TxtFileProvider_Test.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using FluentAssertions;
+using NUnit.Framework;
+using TagCloud.TextProcessing;
+
+namespace TagsCloud_Test
+{
+    public class TxtFileProvider_Test
+    {
+        private TxtFileProvider _fileProvider;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _fileProvider = new TxtFileProvider();
+        }
+
+        [Test]
+        public void GetTxtFilePath_Failure_WhenFileDoesNotExist()
+        {
+            var filePathResult = _fileProvider.GetTxtFilePath("BadFileName");
+            filePathResult.IsSuccess.Should().BeFalse();
+            Console.WriteLine(filePathResult.Error);
+        }
+
+        [Test]
+        public void GetTxtFilePath_IsSuccess_WhenFileExist()
+        {
+            var path = Path.Combine(Path.GetTempPath(), "test.txt");
+            using var writer = new StreamWriter(path, false, Encoding.UTF8);
+            writer.WriteLine("Новый тестовый файл txt");
+            _fileProvider.GetTxtFilePath(path).IsSuccess.Should().BeTrue();
+            File.Delete(path);
+        }
+    }
+}
