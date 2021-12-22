a4df332621eb9cec7ecaab5db3313652709f1d71
Алемасцев Никита <58881824+nikovOne@users.noreply.github.com>
2021-12-22 21:52:52 +0500

    Создал класс TagParser.cs

---
+++ TagsCloudVisualization/TagParser.cs
@@ -0,0 +1,41 @@
+﻿#region
+
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using TagsCloudVisualization.Interfaces;
+
+#endregion
+
+namespace TagsCloudVisualization
+{
+    public class TagParser : ITagParser
+    {
+        private readonly ICloudLayouter cloudLayouter;
+        private readonly Font font;
+        private readonly Graphics graphics;
+
+        public TagParser(ICloudLayouter cloudLayouter, Graphics graphics, Font font)
+        {
+            this.cloudLayouter = cloudLayouter;
+            this.graphics = graphics;
+            this.font = font;
+        }
+
+        public Result<IEnumerable<Tag>> ParseTags(IDictionary<string, int> freqDictionary)
+        {
+            return freqDictionary == null
+                ? new Result<IEnumerable<Tag>>("freqDictionary was null")
+                : new Result<IEnumerable<Tag>>(null, CreateTags(freqDictionary));
+        }
+
+        private IEnumerable<Tag> CreateTags(IDictionary<string, int> freqDictionary)
+        {
+            foreach (var word in freqDictionary.Keys.OrderByDescending(x => freqDictionary[x]))
+            {
+                var rectangle = cloudLayouter.PutNextWord(word, font, graphics);
+                yield return new Tag(rectangle.Value, word, freqDictionary[word]);
+            }
+        }
+    }
+}
