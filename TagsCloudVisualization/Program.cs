bfc684615a400d0a69338363c9bcbbccadf79466
Alex <udovi4@gmail.com>
2021-12-22 18:38:45 +0500

    indentation

--- TagsCloudVisualization/Program.cs
+++ TagsCloudVisualization/Program.cs
@@ -9,13 +9,13 @@ namespace TagsCloudVisualization
 {
     internal static class Program
     {
-         private static void Main(string[] args)
-         { 
-             var tagCloud = new TagCloudFactory().CreateInstance(false, "mixed").Value;
+        private static void Main(string[] args) 
+        {
+            var tagCloud = new TagCloudFactory().CreateInstance(false, "mixed").Value;
             var font = new Font("Comic Sans MS", 9);
             var resolution = new Size(1280, 720);
             tagCloud.CreateTagCloudFromFile("Example.txt", "testimage.png", font,
-                                                    Color.Transparent, 100, resolution);
+                                                Color.Transparent, 100, resolution);
             Process.Start(new ProcessStartInfo("testimage.png")
             { UseShellExecute = true });
         }
