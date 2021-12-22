fddd19c70d63088635c37873a911b6ab564eba33
Алемасцев Никита <58881824+nikovOne@users.noreply.github.com>
2021-12-22 21:53:08 +0500

    Создал интерфейс ITagParser.cs

---
+++ TagsCloudVisualization/Interfaces/ITagParser.cs
@@ -0,0 +1,13 @@
+﻿#region
+
+using System.Collections.Generic;
+
+#endregion
+
+namespace TagsCloudVisualization.Interfaces
+{
+    public interface ITagParser
+    {
+        Result<IEnumerable<Tag>> ParseTags(IDictionary<string, int> words);
+    }
+}
