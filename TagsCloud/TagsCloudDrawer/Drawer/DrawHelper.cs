fa36e96edbcf27ee125e3da9af9c11f012261337
gODealOAple <maksim.math@gmail.com>
2021-12-24 01:26:34 +0500

    Refactor Drawer by introducing helper

---
+++ TagsCloud/TagsCloudDrawer/Drawer/DrawHelper.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using ResultMonad;
+
+namespace TagsCloudDrawer.Drawer
+{
+    public class DrawHelper
+    {
+        private readonly Graphics _graphics;
+        private readonly Rectangle _bounds;
+
+        public DrawHelper(Graphics graphics, Rectangle bounds)
+        {
+            _graphics = graphics;
+            _bounds = bounds;
+        }
+
+        public Result<None> ContinueDraw(Result<None> previousResult, Result<IDrawable> drawableResult) =>
+            from _ in previousResult
+            from drawable in drawableResult
+            from drawResult in Draw(drawable)
+            select drawResult;
+
+        private Result<None> Draw(IDrawable drawable) =>
+            Result.Ok()
+                .Validate(() => _bounds.Contains(drawable.Bounds), "Image cannot contain all tags")
+                .Then(() => Result.Of(() => drawable.Draw(_graphics), "Cannot draw tag"));
+    }
+}
--- TagsCloud/TagsCloudDrawer/Drawer/Drawer.cs
+++ TagsCloud/TagsCloudDrawer/Drawer/Drawer.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using ResultMonad;
@@ -10,23 +9,14 @@ namespace TagsCloudDrawer.Drawer
     {
         public Result<None> Draw(Graphics graphics, Size size, IEnumerable<Result<IDrawable>> drawables)
         {
-            if (graphics == null) throw new ArgumentNullException(nameof(graphics));
             var bounds = new Rectangle(Point.Empty, size);
-            return drawables
-                .Select(tag => tag.Then(t => t.Shift(Size.Truncate(size / 2f))))
-                .DefaultIfEmpty(Result.Fail<IDrawable>("Empty tags"))
-                .Aggregate(Result.Ok(), (prev, curr) => from _ in prev
-                    from drawable in curr
-                    from value in Draw(graphics, drawable, bounds)
-                    select value);
-        }
-
-        private static Result<None> Draw(Graphics graphics, IDrawable drawable, Rectangle bounds)
-        {
-            if (!bounds.Contains(drawable.Bounds))
-                return Result.Fail<None>("Image cannot contain all rectangles");
-            drawable.Draw(graphics);
-            return Result.Ok();
+            var helper = new DrawHelper(graphics, bounds);
+            return Result.Ok()
+                .ValidateNonNull(graphics, nameof(graphics))
+                .Then(() => drawables
+                    .Select(tag => tag.Then(t => t.Shift(Size.Truncate(size / 2f))))
+                    .DefaultIfEmpty(Result.Fail<IDrawable>("Empty tags"))
+                    .Aggregate(Result.Ok(), helper.ContinueDraw));
         }
     }
 }
