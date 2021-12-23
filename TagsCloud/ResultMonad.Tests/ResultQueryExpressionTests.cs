8377201f7352aa0c5b54f4a88575bb98d9365274
gODealOAple <maksim.math@gmail.com>
2021-12-24 03:00:55 +0500

    Refactor ResultEnumerableExtensions.cs and write some tests

--- TagsCloud/ResultMonad.Tests/ResultQueryExpressionTests.cs
+++ TagsCloud/ResultMonad.Tests/ResultQueryExpressionTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -82,6 +83,70 @@ namespace ResultMonad.Tests
                 from guid in (hex + hex + hex + hex).ParseGuidResult()
                 select guid;
             parseResult.Should().BeEquivalentTo(Result.Fail<Guid>("error is here"));
+        }
+    }
+
+    [TestFixture]
+    public class ResultEnumerableExtensionsTests
+    {
+        [Test]
+        public void Traverse_ShouldHasValue_WhenCollectionWithNoError()
+        {
+            var numbers = new[] { 1, 2, 3 };
+            var results = numbers.Select(Result.Ok);
+
+            var traversed = results.Traverse();
+
+            traversed.GetValueOrThrow().Should().ContainInOrder(numbers);
+        }
+
+        [Test]
+        public void Traverse_ShouldHasError_WhenMiddleElementWithError()
+        {
+            var fail = Result.Fail<int>("Error");
+            var results = new[]
+            {
+                1.AsResult(),
+                fail,
+                2.AsResult()
+            };
+
+            var traversed = results.Traverse();
+
+            traversed.IsSuccess.Should().BeFalse();
+            traversed.Error.Should().NotBeNull();
+        }
+
+        [Test]
+        public void Traverse_ShouldHasOnlyFirstError_WhenMoreThanOneErrorInSource()
+        {
+            var fail = Result.Fail<int>("Error");
+            var otherFail = Result.Fail<int>("Error2");
+            var results = new[]
+            {
+                fail,
+                otherFail
+            };
+
+            var traversed = results.Traverse();
+
+            traversed.Error.Should().Be(fail.Error);
+        }
+
+        [Test]
+        public void Traverse_ShouldNotEnumerate_WhenFoundError()
+        {
+            var fail = Result.Fail<int>("Error");
+            var count = 0;
+            var results = new[] { fail, 1, 2 }.Select(element =>
+            {
+                count++;
+                return element;
+            });
+
+            results.Traverse();
+
+            count.Should().Be(1);
         }
     }
 }
--- TagsCloud/ResultMonad/ResultEnumerableExtensions.cs
+++ TagsCloud/ResultMonad/ResultEnumerableExtensions.cs
@@ -4,17 +4,22 @@ namespace ResultMonad
 {
     public static class ResultEnumerableExtensions
     {
-        public static Result<IEnumerable<TInput>> Traverse<TInput>(this IEnumerable<Result<TInput>> collection)
+        public static Result<IEnumerable<TInput>> Traverse<TInput>(this IEnumerable<Result<TInput>> source)
         {
-            var list = new List<TInput>();
-            foreach (var value in collection)
-            {
-                if (!value.IsSuccess)
-                    return Result.Fail<IEnumerable<TInput>>(value.Error);
-                list.Add(value.Value);
-            }
+            return Result.Ok()
+                .Validate(() => source is not null, $"{nameof(source)} was null")
+                .Then(() =>
+                {
+                    var results = new List<TInput>();
+                    foreach (var element in source)
+                    {
+                        if (!element.IsSuccess)
+                            return Result.Fail<IEnumerable<TInput>>(element.Error);
+                        results.Add(element.Value);
+                    }
 
-            return Result.Ok<IEnumerable<TInput>>(list);
+                    return results;
+                });
         }
     }
 }
