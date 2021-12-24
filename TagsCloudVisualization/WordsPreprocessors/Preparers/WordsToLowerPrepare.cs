3c637ebf6efe828fba60c7a3291384aa78c4dce6
IvanSibirtsev <sibirtsev.ivan@gmail.com>
2021-12-24 23:17:58 +0500

    WordsToLowerPrepare: ToLower -> ToLowerInvariant

--- TagsCloudVisualization/WordsPreprocessors/Preparers/WordsToLowerPrepare.cs
+++ TagsCloudVisualization/WordsPreprocessors/Preparers/WordsToLowerPrepare.cs
@@ -6,6 +6,6 @@ namespace TagsCloudVisualization.WordsPreprocessors.Preparers
     public class WordsToLowerPrepare : IWordsPreparer
     {
         public IEnumerable<string> Prepare(IEnumerable<string> words) =>
-            words.Select(word => word.ToLower());
+            words.Select(word => word.ToLowerInvariant());
     }
 }
