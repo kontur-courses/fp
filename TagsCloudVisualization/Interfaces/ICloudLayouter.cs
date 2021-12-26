8fcab0725229129d003bce7bba05404e72d7d8fb
Алемасцев Никита <58881824+nikovOne@users.noreply.github.com>
2021-12-26 19:14:04 +0500

    Удалил лишние методы

--- TagsCloudVisualization/Interfaces/ICloudLayouter.cs
+++ TagsCloudVisualization/Interfaces/ICloudLayouter.cs
@@ -1,13 +1,11 @@
-﻿using System.Collections.Generic;
-using System.Drawing;
+﻿using System.Drawing;
 
 namespace TagsCloudVisualization.Interfaces
 {
     public interface ICloudLayouter
     {
-        Result<Rectangle> PutNextWord(string word, Font font, Graphics graphics);
-        Result<List<Rectangle>> PutWords(IEnumerable<string> words, Font font, Graphics graphics);
+        Result<Rectangle> PutNextRectangle(Size size);
         Result<Rectangle[]> GetPutRectangles();
-        Result<Point> GetCenter();
+        Point GetCenter();
     }
 }
