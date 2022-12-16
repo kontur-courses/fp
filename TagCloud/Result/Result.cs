5cef6db9c89ca645a9556912364eb61f96973e40
VladislavSharonov <fydoooooo@gmail.com>
2022-12-16 12:47:09 +0500

    Added result class

---
+++ TagCloud/Result/Result.cs
@@ -0,0 +1,16 @@
+﻿namespace Result;
+
+public class Result<T>
+{
+    public string? Error { get; }
+    public T? Value { get; }
+    public bool IsSuccess => Error is null;
+
+    public Result(T? value, string? error = null)
+    {
+        Error = error;
+        Value = value;
+    }
+
+    public static implicit operator T?(Result<T?> result) => result.Value;
+}
