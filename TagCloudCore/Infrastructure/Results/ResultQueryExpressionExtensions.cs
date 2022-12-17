4ccb27882f05e0dd4a1dea57b6d8fab81ce114c1
Defalt <ilyaudalov1@mail.ru>
2022-12-18 01:43:03 +0500

    fix(ResultQueryExpressionExtensions): nullable args removed

--- TagCloudCore/Infrastructure/Results/ResultQueryExpressionExtensions.cs
+++ TagCloudCore/Infrastructure/Results/ResultQueryExpressionExtensions.cs
@@ -4,16 +4,16 @@ public static class ResultQueryExpressionExtensions
 {
     public static Result<TOutput> SelectMany<TInput, TOutput>(
         this Result<TInput> input,
-        Func<TInput?, Result<TOutput>> continuation
+        Func<TInput, Result<TOutput>> continuation
     ) =>
         input.Then(continuation);
 
     public static Result<TSelected> SelectMany<TInput, TOutput, TSelected>(
         this Result<TInput> input,
-        Func<TInput?, Result<TOutput>> continuation,
-        Func<TInput?, TOutput?, TSelected> resultSelector)
+        Func<TInput, Result<TOutput>> continuation,
+        Func<TInput, TOutput, TSelected> resultSelector)
     {
         return input.Then(continuation)
-            .Then(o => resultSelector(input.Value, o));
+            .Then(o => resultSelector(input.Value!, o));
     }
 }
