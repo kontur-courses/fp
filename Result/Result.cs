3849d1c1ce7644db3fe4b0e8e2b2cb5fc23312ae
Art <bartalamu@list.ru>
2022-12-19 18:23:45 +0500

    Result project - csproj - Result class

---
+++ Result/Result.cs
@@ -0,0 +1,12 @@
+﻿namespace Result;
+
+public class Result<T>
+{
+    public T Value { get; protected set; }
+    public Exception? Exception { get; protected set; }
+    public bool IsSuccess => Exception == null;
+
+    public static implicit operator bool(Result<T> result) => result.IsSuccess;
+    public Result(T value) => Value = value;
+    public Result(Exception error) => Exception = error;
+}
