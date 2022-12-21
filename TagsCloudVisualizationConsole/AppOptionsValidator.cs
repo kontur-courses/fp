51a347e021be2f0c12e172f9802faa58d9be7e06
max <glushakovmax32@gmail.com>
2022-12-21 23:48:35 +0300

    use nullable type in ArgsOptions and AppOptionsValidator

--- TagsCloudVisualizationConsole/AppOptionsValidator.cs
+++ TagsCloudVisualizationConsole/AppOptionsValidator.cs
@@ -8,10 +8,10 @@ public static class AppOptionsValidator
     public static Result<ArgsOptions> ValidatePathsInOptions(ArgsOptions argsOptions)
     {
         if (!File.Exists(argsOptions.PathToTextFile))
-            return Result.Fail<ArgsOptions>($"{argsOptions.PathToTextFile} does not exist. Check or create file.");
+            return Result.Fail<ArgsOptions>($"File \"{argsOptions.PathToTextFile}\" does not exist. Check or create file.");
 
         if (!Directory.Exists(argsOptions.DirectoryToSaveFile))
-            return Result.Fail<ArgsOptions>($"{argsOptions.DirectoryToSaveFile} does not exist. Check path or create directory.");
+            return Result.Fail<ArgsOptions>($"Directory \"{argsOptions.DirectoryToSaveFile}\" does not exist. Check path or create directory.");
 
         if (string.IsNullOrEmpty(argsOptions.SaveFileName))
             return Result.Fail<ArgsOptions>($"Save file name empty. Pass file name to save result image by SaveFileName parameter");
@@ -20,7 +20,7 @@ public static class AppOptionsValidator
     }
 
 
-    public static Result<ImageFormat> GetImageFormat(string format)
+    public static Result<ImageFormat> GetImageFormat(string? format)
     {
         return format.ToLower() switch
         {
