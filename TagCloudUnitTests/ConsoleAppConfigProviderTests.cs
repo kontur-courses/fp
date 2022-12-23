589d543e89f9c6f0efdfb0b1c4790903fd2dd115
Viktor Sotsuk <sotsukv@gmail.com>
2022-12-23 20:11:00 +0400

    ConsoleAppConfigProviderTests.cs was added

---
+++ TagCloudUnitTests/ConsoleAppConfigProviderTests.cs
@@ -0,0 +1,50 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TagCloud.AppConfiguration;
+
+namespace TagCloudUnitTests
+{
+    [TestFixture]
+    internal class ConsoleAppConfigProviderTests
+    {
+        private ConsoleAppConfigProvider provider;
+
+        private string args;
+
+        [SetUp]
+        public void SetUp()
+        {
+            provider = new ConsoleAppConfigProvider();
+
+            args = "-i Text.txt -o Output.png";
+        }
+
+        [TestCase(" -z InvalidCloudForm", TestName = "Invalid cloud form")]
+        [TestCase(" -w -100 -h 100", TestName = "Image width is negative")]
+        [TestCase(" -w 100 -h -100", TestName = "Image height is negative")]
+        [TestCase(" -w 0 -h 100", TestName = "Image width is zero")]
+        [TestCase(" -w 100 -h 0", TestName = "Image height is zero")]
+        [TestCase(" -b InvalidBackgroundColor", TestName = "Invalid background color")]
+        [TestCase(" -f InvalidFontFamily", TestName = "Invalid font family")]
+        [TestCase(" -l -10 -p 100", TestName = "Min font size is negative")]
+        [TestCase(" -l 10 -p -100", TestName = "Max font size is negative")]
+        [TestCase(" -l 0 -p 100", TestName = "Min font size is zero")]
+        [TestCase(" -l 10 -p 0", TestName = "Max font size is zero")]
+        [TestCase(" -l 10 -p 5", TestName = "Min font size is greater than max font size")]
+        [TestCase(" -k InvalidWordColoring", TestName = "Invalid word coloring")]
+        public void Test(string testingsArgs)
+        {
+            args += testingsArgs;
+
+            var config = new ConsoleAppConfigProvider().GetAppConfig(args.Split(' '));
+
+            config.IsSuccess.Should().BeFalse();
+        }
+
+    }
+}
