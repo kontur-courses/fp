ffb39cefed47b794d5e3245d561e9560186040ae
Seekard <maks_byx@mail.ru>
2022-12-24 02:18:06 +0300

    Refactor to method chaining

--- TagCloudResult/Clients/Client.cs
+++ TagCloudResult/Clients/Client.cs
@@ -20,45 +20,34 @@ public class Client
         _saver = saver;
     }
 
-    public Result<Bitmap> Draw(IEnumerable<Word> words, ICurve curve, Size size, Font font, IEnumerable<Color> colors)
+    public Bitmap Draw(IEnumerable<Word> words, ICurve curve, Size size, Font font, IEnumerable<Color> colors)
     {
         var wordRectangles = new List<WordRectangle>();
         var fontSize = font.Size;
         foreach (var word in words.OrderByDescending(word => word.Frequency))
         {
-            font = font.ChangeSize(fontSize * word.Frequency);
-            var rectangleSize = word.MeasureWord(font);
-            var rectangle = _layouter.PutRectangle(curve, rectangleSize);
-            if (rectangle.IsSuccess == false)
-                return Result.Fail<Bitmap>(rectangle.Error).RefineError("Couldn't draw image.");
-            var wordRectangle = new WordRectangle(word) { Rectangle = rectangle.Value };
-            wordRectangles.Add(wordRectangle);
+            var wordRectangle = Result.Of(() => font.ChangeSize(fontSize * word.Frequency))
+                .Then(word.MeasureWord)
+                .Then(rectangleSize => _layouter.PutRectangle(curve, rectangleSize))
+                .Then(rectangle => new WordRectangle(word) { Rectangle = rectangle })
+                .OnFail(PrintError);
+            wordRectangles.Add(wordRectangle.Value);
         }
 
         font = font.ChangeSize(fontSize);
-        var image = _drawer.CreateImage(wordRectangles, size, font, colors);
-        return image;
+        var image = _drawer.CreateImage(wordRectangles, size, font, colors).OnFail(PrintError);
+        return image.Value;
     }
 
-    public Result<None> Save(Bitmap image, IEnumerable<string> destinationPaths)
+    public void Save(Bitmap image, IEnumerable<string> destinationPaths)
     {
-        foreach (var destinationPath in destinationPaths)
-        {
-            var result = Save(image, destinationPath);
-            if (result.IsSuccess == false)
-                return result;
-        }
-
-        return new Result<None>();
+        foreach (var destinationPath in destinationPaths) Save(image, destinationPath).OnFail(PrintError);
     }
 
     public Result<None> Save(Bitmap image, string destinationPath)
     {
-        var format = Helper.GetImageFormat(destinationPath);
-        if (format.IsSuccess == false)
-            return Result.Fail<None>("Cannot define image extension.");
-        var result = _saver.Save(image, destinationPath, format.Value);
-        return result;
+        return Helper.GetImageFormat(destinationPath)
+            .Then(format => _saver.Save(image, destinationPath, format));
     }
 
     public void PrintError(string error)
