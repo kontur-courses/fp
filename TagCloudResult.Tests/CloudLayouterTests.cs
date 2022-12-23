ae0d64fecad24ace8676cbea7491a23e8db926d7
Seekard <maks_byx@mail.ru>
2022-12-23 18:42:26 +0300

    Added tests from previous hw

---
+++ TagCloudResult.Tests/CloudLayouterTests.cs
@@ -0,0 +1,64 @@
+﻿using System.Drawing;
+using TagCloudResult.Curves;
+
+namespace TagCloudResult.Tests;
+
+[TestFixture]
+public class CloudLayouterTests
+{
+    
+    [SetUp] 
+    public void SetUp()
+    {
+        _curve = new ArchimedeanSpiral();
+        _layouter = new CloudLayouter();
+    }
+
+    private CloudLayouter _layouter;
+    private ICurve _curve;
+
+    private void PutAllRectangles(IReadOnlyList<Size> rectangles)
+    {
+        foreach (var rectangleSize in rectangles)
+            _layouter.PutRectangle(_curve, rectangleSize);
+    }
+
+    private static IEnumerable<TestCaseData> SizesOfRectangles
+    {
+        get
+        {
+            yield return new TestCaseData(
+                new List<Size>
+                {
+                    new(100, 100),
+                    new(100, 100),
+                    new(100, 100),
+                    new(100, 100),
+                    new(100, 100)
+                });
+            yield return new TestCaseData(
+                new List<Size>
+                {
+                    new(100, 100),
+                    new(200, 100),
+                    new(100, 200),
+                    new(1000, 1000),
+                    new(100, 100)
+                });
+        }
+    }
+
+    [TestCaseSource(nameof(SizesOfRectangles))]
+    public void PutNextRectangle_ShouldNotMakeIntersections(IReadOnlyList<Size> sizesOfRectangles)
+    {
+        PutAllRectangles(sizesOfRectangles);
+
+        foreach (var rectangle1 in _layouter.Rectangles)
+        foreach (var rectangle2 in _layouter.Rectangles)
+        {
+            if (rectangle1 == rectangle2)
+                continue;
+            rectangle1.IntersectsWith(rectangle2).Should().BeFalse();
+        }
+    }
+}
---
+++ TagCloudResult.Tests/Usings.cs
@@ -0,0 +1,3 @@
+global using NUnit.Framework;
+global using TagCloudResult;
+global using FluentAssertions;
