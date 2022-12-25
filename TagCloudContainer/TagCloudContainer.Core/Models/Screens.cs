9ac3556b6f472bf7bb8a2858b34ced1cadf3151b
Luvr681 <secondgoldvip@gmail.com>
2022-12-26 01:15:03 +0500

    Выделил необходимые модели

---
+++ TagCloudContainer/TagCloudContainer.Core/Models/Screens.cs
@@ -0,0 +1,18 @@
+﻿namespace TagCloudContainer.Core.Models;
+
+public static class Screens
+{
+    private static readonly Size[] _sizes = 
+    {
+        new Size(1920, 1080),
+        new Size(1600, 900),
+        new Size(1400, 1050),
+        new Size(1024, 768),
+        new Size(600, 600),
+    };
+
+    public static Size[] Sizes
+    {
+        get => _sizes;
+    }
+}
