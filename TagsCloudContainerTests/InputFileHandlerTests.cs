e5c09ffc55391243b35b710db12f490e92ad46c3
massshasch <massshasch@gmail.com>
2022-12-25 12:44:42 +0500

    One more fix

--- TagsCloudContainerTests/InputFileHandlerTests.cs
+++ TagsCloudContainerTests/InputFileHandlerTests.cs
@@ -1,44 +1,17 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Text;
-using CommandLine;
 using FluentAssertions;
 using NUnit.Framework;
 using TagCloudContainer;
-using TagCloudContainer.FileReaders;
-using TagCloudContainer.UI;
 
 namespace TagsCloudContainerTests
 {
     public class InputFileHandlerTests
     {
-        private string path;
-        private FileStream fs;
-        private string[] words;
-        private IUi settings;
-
-        [SetUp]
-        public void SetUp()
-        {
-            path = "../testfile.txt";
-            words = new TxtReader().FileToWordsArray(path).Value;
-            fs = new FileStream(path, FileMode.Create);
-            settings = Parser.Default.ParseArguments<ConsoleUiSettings>(new string[] { }).Value;
-            fs.Dispose();
-        }
-
-
         [Test]
         public void FormFrequencyDictionary_ShouldСorrectFormFrequencyDictionary()
         {
-            fs = File.OpenWrite(path);
-            var data = "mary\nbloody\nmary\njune";
-            byte[] bytes = Encoding.UTF8.GetBytes(data);
-            fs.Write(bytes, 0, bytes.Length);
-            fs.Dispose();
-
-            var result = InputFileHandler.FormFrequencyDictionary(words);
+            var result = InputFileHandler.FormFrequencyDictionary(new[] {"mary", "bloody", "Mary", "JUNE"});
             result.Value.Should().BeEquivalentTo(new Dictionary<string, int> {{"mary", 2}, {"bloody", 1}, {"june", 1}});
         }
 
