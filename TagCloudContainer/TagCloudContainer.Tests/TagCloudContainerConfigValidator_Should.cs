e765b9f565517008bf2dec71430588dccdd57c3f
Luvr681 <secondgoldvip@gmail.com>
2022-12-26 01:11:32 +0500

    Добавил Тесты на основную валидацию конфигурационных данных

---
+++ TagCloudContainer/TagCloudContainer.Tests/TagCloudContainerConfigValidator_Should.cs
@@ -0,0 +1,103 @@
+﻿using System.Drawing;
+using FluentAssertions;
+using NUnit.Framework;
+using TagCloudContainer.Configs;
+using TagCloudContainer.Core;
+using TagCloudContainer.Core.Interfaces;
+using TagCloudContainer.Forms.Validators;
+
+namespace TagCloudContainer.Tests;
+
+[TestFixture]
+public class TagCloudContainerConfigValidator_Should
+{
+    [Test]
+    public void ValidateContainerConfig_CorrectParameters_ShouldBeOk()
+    {
+        var validator = new TagCloudContainerConfigValidator();
+        var tagCloudContainerConfig = new TagCloudContainerConfig();
+
+        validator
+            .Validate(tagCloudContainerConfig)
+            .IsSuccess
+            .Should()
+            .BeTrue();
+    }
+    
+    [Test]
+    public void ValidateContainerConfig_IncorrectMainDirectoryPath_ShouldBeFail()
+    {
+        var validator = new TagCloudContainerConfigValidator();
+        var tagCloudContainerConfig = new TagCloudContainerConfig()
+        {
+            MainDirectoryPath = Path.Combine(TagCloudContainerConfig.GetMainDirectoryPath(), Guid.NewGuid().ToString("N"))
+        };
+        
+        validator
+            .Validate(tagCloudContainerConfig)
+            .Should()
+            .BeEquivalentTo(Result.Fail<ITagCloudContainerConfig>("Incorrect path to main directory"));
+    }
+    
+    [Test]
+    public void ValidateContainerConfig_Null_ShouldBeFail()
+    {
+        var validator = new TagCloudContainerConfigValidator();
+        
+        validator
+            .Validate(null)
+            .Should()
+            .BeEquivalentTo(Result.Fail<ITagCloudContainerConfig>("Tag cloud config is null"));
+    }
+    
+    [Test]
+    public void ValidateContainerConfig_EmptyCenterPoint_ShouldBeFail()
+    {
+        var validator = new TagCloudContainerConfigValidator();
+        var tagCloudContainerConfig = new TagCloudContainerConfig() { Center = Point.Empty };
+        
+        validator
+            .Validate(tagCloudContainerConfig)
+            .Should()
+            .BeEquivalentTo(Result.Fail<ITagCloudContainerConfig>("Center point can't be empty"));
+    }
+    
+    [Test]
+    public void ValidateContainerConfig_EmptyStandartSize_ShouldBeFail()
+    {
+        var validator = new TagCloudContainerConfigValidator();
+        var tagCloudContainerConfig = new TagCloudContainerConfig() { StandartSize = Size.Empty };
+
+        validator
+            .Validate(tagCloudContainerConfig)
+            .Should()
+            .BeEquivalentTo(Result.Fail<ITagCloudContainerConfig>("Standart size can't be empty"));
+    }
+    
+    [Test]
+    public void ValidateWordReaderConfig_IncorrectFilePath_ShouldBeFail()
+    {
+        var validator = new TagCloudContainerConfigValidator();
+        var tagCloudContainerConfig = new TagCloudContainerConfig();
+        tagCloudContainerConfig.SetFilePath("test.cs");
+
+        validator
+            .Validate(tagCloudContainerConfig)
+            .Should()
+            .BeEquivalentTo(Result.Fail<ITagCloudContainerConfig>("Words file does not exist"));
+    }
+    
+    [Test]
+    public void ValidateWordReaderConfig_IncorrectExcludingWordsFilePath_ShouldBeFail()
+    {
+        var validator = new TagCloudContainerConfigValidator();
+        var tagCloudContainerConfig = new TagCloudContainerConfig();
+        tagCloudContainerConfig.SetExcludeWordsFilePath("");
+
+        validator
+            .Validate(tagCloudContainerConfig)
+            .Should()
+            .BeEquivalentTo(Result.Fail<ITagCloudContainerConfig>("Exclude words file does not exist"));
+    }
+    
+}
---
+++ TagCloudContainer/TagCloudContainer.Tests/TagCloudFormConfigValidator_Should.cs
@@ -0,0 +1,61 @@
+﻿using System.Drawing;
+using FluentAssertions;
+using NUnit.Framework;
+using TagCloudContainer.Configs;
+using TagCloudContainer.Core;
+using TagCloudContainer.Core.Interfaces;
+using TagCloudContainer.Forms.Validators;
+
+namespace TagCloudContainer.Tests;
+
+[TestFixture]
+public class TagCloudFormConfigValidator_Should
+{
+    [Test]
+    public void ValidateTagCloudFormConfig_CorrectParameters_ShouldBeOk()
+    {
+        var validator = new TagCloudFormConfigValidator();
+        var tagCloudFormConfig = new TagCloudContainerConfig();
+
+        validator
+            .Validate(tagCloudFormConfig)
+            .IsSuccess
+            .Should()
+            .BeTrue();
+    }
+    
+    [Test]
+    public void ValidateTagCloudFormConfig_TryValidateNull_ShouldBeFail()
+    {
+        var validator = new TagCloudFormConfigValidator();
+
+        validator
+            .Validate(null)
+            .Should()
+            .BeEquivalentTo(Result.Fail<ITagCloudFormConfig>("Tag cloud form config is null"));
+    }
+    
+    [Test]
+    public void ValidateTagCloudFormConfig_EmptyFormSize_ShouldBeFail()
+    {
+        var validator = new TagCloudFormConfigValidator();
+        var tagCloudFormConfig = new TagCloudContainerConfig() { ImageSize = Size.Empty };
+
+        validator
+            .Validate(tagCloudFormConfig)
+            .Should()
+            .BeEquivalentTo(Result.Fail<ITagCloudFormConfig>("Form size can't be empty or null"));
+    }
+    
+    [Test]
+    public void ValidateTagCloudFormConfig_InvalidFontFamily_ShouldBeFail()
+    {
+        var validator = new TagCloudFormConfigValidator();
+        var tagCloudFormConfig = new TagCloudContainerConfig() { FontFamily = Guid.NewGuid().ToString("N") };
+
+        validator
+            .Validate(tagCloudFormConfig)
+            .Should()
+            .BeEquivalentTo(Result.Fail<ITagCloudFormConfig>("Incorrect font family"));
+    }
+}
