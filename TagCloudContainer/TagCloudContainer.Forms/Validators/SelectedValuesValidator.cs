af248bc8bf089846b81b9d73d680683c668e81b4
Luvr681 <secondgoldvip@gmail.com>
2022-12-26 17:20:22 +0500

    Добавил валидацию для новых конфигов

---
+++ TagCloudContainer/TagCloudContainer.Forms/Validators/SelectedValuesValidator.cs
@@ -0,0 +1,21 @@
+﻿using System.Drawing.Text;
+using TagCloudContainer.Core;
+using TagCloudContainer.Core.Interfaces;
+using TagCloudContainer.Forms.Interfaces;
+
+namespace TagCloudContainer.Forms.Validators;
+
+public class SelectedValuesValidator : IConfigValidator<ISelectedValues>
+{
+    public Result<ISelectedValues> Validate(ISelectedValues selectedValues)
+    {
+        var systemFonts = new InstalledFontCollection().Families.Select(f => f.Name);
+        
+        if (selectedValues.ImageSize.IsEmpty || selectedValues.ImageSize == null) 
+            return Result.Fail<ISelectedValues>("Form size can't be empty or null");
+        if (!systemFonts.Contains(selectedValues.FontFamily)) 
+            return Result.Fail<ISelectedValues>("Incorrect font family");
+        
+        return Result.Ok(selectedValues);
+    }
+}
