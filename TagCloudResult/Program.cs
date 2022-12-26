da2361fc19d7420e1b88a00ef5d9f92e7f8022e3
Seekard <maks_byx@mail.ru>
2022-12-26 16:44:08 +0300

    Moved PrintError into Program Made arguments parse before building container

--- TagCloudResult/Program.cs
+++ TagCloudResult/Program.cs
@@ -1,5 +1,4 @@
-﻿using CommandLine;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using ResultOfTask;
 using TagCloudResult.Clients;
 using TagCloudResult.Files;
@@ -11,22 +10,27 @@ internal static class Program
 {
     public static void Main(string[] args)
     {
+        var parsedArguments = CommandLineOptions.Parse(args).OnFail(PrintError).Value.Value;
         var serviceProvider = DiContainerConfiguration.Build();
         var textFormatter = serviceProvider.GetRequiredService<TextFormatter>();
         var fileReader = serviceProvider.GetRequiredService<FileReader>();
         var client = serviceProvider.GetRequiredService<Client>();
-        var parsedArguments = Result.Of(() => Parser.Default.ParseArguments<CommandLineOptions>(args).Value)
-            .OnFail(client.PrintError).Value;
 
-        client.Curve = Helper.GetCurveByName(parsedArguments.Curve).OnFail(client.PrintError).Value;
-        client.Font = Helper.GetFont(parsedArguments.FontFamily, parsedArguments.FontSize).OnFail(client.PrintError)
+        client.Curve = Helper.GetCurveByName(parsedArguments.Curve).OnFail(PrintError).Value;
+        client.Font = Helper.GetFont(parsedArguments.FontFamily, parsedArguments.FontSize).OnFail(PrintError)
             .Value;
-        client.ImageSize = Helper.GetSize(parsedArguments.Width, parsedArguments.Height).OnFail(client.PrintError)
+        client.ImageSize = Helper.GetSize(parsedArguments.Width, parsedArguments.Height).OnFail(PrintError)
             .Value;
         fileReader.ReadAll(parsedArguments.InputFile)
             .Then(textFormatter.Format)
             .Then(words => client.Draw(words, parsedArguments.Colors))
             .Then(image => client.Save(image, parsedArguments.OutputFiles))
-            .OnFail(client.PrintError);
+            .OnFail(PrintError);
+    }
+
+    public static void PrintError(string error)
+    {
+        Console.WriteLine(error);
+        Environment.Exit(1);
     }
 }
