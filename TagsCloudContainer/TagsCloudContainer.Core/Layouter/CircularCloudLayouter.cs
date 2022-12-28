5f44a3608eaefb7cd27b22ca6988d4461b2adfc4
Source34 <rom.steam2010@yandex.ru>
2022-12-28 18:16:42 +0300

    Update GetDistance

--- TagsCloudContainer/TagsCloudContainer.Core/Layouter/CircularCloudLayouter.cs
+++ TagsCloudContainer/TagsCloudContainer.Core/Layouter/CircularCloudLayouter.cs
@@ -91,7 +91,7 @@ namespace TagsCloudContainer.Core.Layouter
 
         private static double GetDistance(Point p1, Point p2)
         {
-            return Math.Sqrt(Math.Pow(p2.X - p1.X, 2) + Math.Pow(p2.Y - p1.Y, 2));
+            return Math.Sqrt((p2.X - p1.X) * (p2.X - p1.X) + (p2.Y - p1.Y) * (p2.Y - p1.Y));
         }
     }
 }
