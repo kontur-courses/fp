65bd84498ea922769f9b1c8b7e67951ec6ba03a0
batyadmx <andronovaleksandrptg@yandex.ru>
2024-02-02 21:09:46 +0500

    Import project

---
+++ TagCloud.Tests/FrequencyDictionaryBuilderTests.cs
@@ -0,0 +1,32 @@
+﻿using FluentAssertions;
+
+namespace TagCloud.Tests
+{
+    [TestFixture]
+    public class FrequencyDictionaryBuilderTests
+    {
+        private FrequencyDictionaryBuilder<string> builder;
+
+        [SetUp]
+        public void Setup()
+        {
+            builder = new FrequencyDictionaryBuilder<string>();
+        }
+
+        [Test]
+        public void Build_PassValidArray_BuildsCorrectDict()
+        {
+            var words = new[] { "Машина", "Самолет", "Велосипед", "Велосипед" };
+            var correctDict = new Dictionary<string, int>() 
+            {
+                ["Машина"] = 1,
+                ["Самолет"] = 1,
+                ["Велосипед"] = 2
+            };
+
+            var dict = builder.Build(words);
+
+            dict.Should().BeEquivalentTo(correctDict);
+        }
+    }
+}
