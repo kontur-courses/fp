cc5b0bc55e11172a5d2a930b50a7b8a57fbe299f
daHil <ssocmain@gmail.com>
2024-02-04 16:46:50 +0500

    added tests for ArchimedeanSpiralPointer

--- Tests/ArchimedeanSpiralTests.cs
+++ Tests/ArchimedeanSpiralTests.cs
@@ -8,25 +8,6 @@ namespace TagsCloudPainterTests;
 [TestFixture]
 public class ArchimedeanSpiralTests
 {
-    private static IEnumerable<TestCaseData> GetNextPointFails => new[]
-    {
-        new TestCaseData(new Point(1, 1), 0, 1, 1).SetName("WhenGivenNotPositiveStep"),
-        new TestCaseData(new Point(1, 1), 1, 0, 1).SetName("WhenGivenNotPositiveRadius"),
-        new TestCaseData(new Point(1, 1), 1, 1, 0).SetName("WhenGivenNotPositiveAngle")
-    };
-
-    [TestCaseSource(nameof(GetNextPointFails))]
-    public void GetNextPoint_ShouldFail(Point center, double step, double radius, double angle)
-    {
-        var cloudSettings = new CloudSettings { CloudCenter = center };
-        var pointerSettings = new SpiralPointerSettings { AngleConst = angle, RadiusConst = radius, Step = step };
-        var pointer = new ArchimedeanSpiralPointer(cloudSettings, pointerSettings);
-
-        var result = pointer.GetNextPoint();
-        
-        Assert.That(result.IsSuccess, Is.False);
-    }
-
     private static IEnumerable<TestCaseData> ConstructorArgumentExceptions => new[]
     {
         new TestCaseData(null, new SpiralPointerSettings()).SetName("WhenGivenNullCloudSettings"),
@@ -38,5 +19,55 @@ public class ArchimedeanSpiralTests
     public void Constructor_ShouldThrowArgumentException_(ICloudSettings cloudSettings, ISpiralPointerSettings pointerSettings)
     {
         Assert.Throws<ArgumentNullException>(() => new ArchimedeanSpiralPointer(cloudSettings, pointerSettings));
+    }
+
+    private static IEnumerable<TestCaseData> GetNextPointFails => new[]
+    {
+        new TestCaseData(new Point(1, 1), 0, 1, 1).SetName("WhenGivenNotPositiveStep"),
+        new TestCaseData(new Point(1, 1), 1, 0, 1).SetName("WhenGivenNotPositiveRadius"),
+        new TestCaseData(new Point(1, 1), 1, 1, 0).SetName("WhenGivenNotPositiveAngle")
+    };
+
+    [TestCaseSource(nameof(GetNextPointFails))]
+    public void GetNextPoint_ShouldFail_(Point center, double step, double radius, double angle)
+    {
+        var cloudSettings = new CloudSettings { CloudCenter = center };
+        var pointerSettings = new SpiralPointerSettings { AngleConst = angle, RadiusConst = radius, Step = step };
+        var pointer = new ArchimedeanSpiralPointer(cloudSettings, pointerSettings);
+
+        var result = pointer.GetNextPoint();
+
+        Assert.That(result.IsSuccess, Is.False);
+    }
+
+    private static IEnumerable<TestCaseData> GetNextPointSuccess => new[]
+    {
+        new TestCaseData(new Point(1, 1), 1, 1, 1).SetName("WhenGivenPositiveStepAndRadiusAndAngle"),
+        new TestCaseData(new Point(1, 1), 100, 100, 100).SetName("WhenGivenPositiveStepAndRadiusAndAngleOfLargeNumbers"),
+    };
+
+    [TestCaseSource(nameof(GetNextPointSuccess))]
+    public void GetNextPoint_ShouldSuccess_(Point center, double step, double radius, double angle)
+    {
+        var cloudSettings = new CloudSettings { CloudCenter = center };
+        var pointerSettings = new SpiralPointerSettings { AngleConst = angle, RadiusConst = radius, Step = step };
+        var pointer = new ArchimedeanSpiralPointer(cloudSettings, pointerSettings);
+
+        var result = pointer.GetNextPoint();
+
+        Assert.That(result.IsSuccess, Is.True);
+    }
+
+    [Test]
+    public void GetNextPoint_ShouldReturnPointThatDiffersFromPreviousPoint()
+    {
+        var cloudSettings = new CloudSettings { CloudCenter = new Point(1, 1) };
+        var pointerSettings = new SpiralPointerSettings { AngleConst = 1, RadiusConst = 1, Step = 1 };
+        var pointer = new ArchimedeanSpiralPointer(cloudSettings, pointerSettings);
+
+        var firstPoint = pointer.GetNextPoint().GetValueOrThrow();
+        var secondPoint = pointer.GetNextPoint().GetValueOrThrow();
+
+        Assert.That(firstPoint, Is.Not.EqualTo(secondPoint));
     }
 }
