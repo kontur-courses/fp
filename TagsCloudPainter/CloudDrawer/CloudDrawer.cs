2f5ff05346ec247f6ffe14f05fec164bf1a470f6
daHil <ssocmain@gmail.com>
2024-02-04 19:42:58 +0500

    wrapped interaction with Drawing in Result

--- TagsCloudPainter/CloudDrawer/CloudDrawer.cs
+++ TagsCloudPainter/CloudDrawer/CloudDrawer.cs
@@ -24,29 +24,45 @@ public class CloudDrawer : ICloudDrawer
         if (imageWidth <= 0 || imageHeight <= 0)
             return Result.Fail<Bitmap>("either width or height of image is not positive");
 
-        var drawingScale = CalculateObjectDrawingScale(cloud.GetWidth(), cloud.GetHeight(), imageWidth, imageHeight);
-        if (!drawingScale.IsSuccess)
-            return Result.Fail<Bitmap>(drawingScale.Error);
+        var pen = Result.Of(() => new Pen(tagSettings.TagColor));
+        if (!pen.IsSuccess)
+            return Result.Fail<Bitmap>(pen.Error);
 
-        var bitmap = new Bitmap(imageWidth, imageHeight);
-        using var graphics = Graphics.FromImage(bitmap);
-        using var pen = new Pen(tagSettings.TagColor);
-        {
-            graphics.TranslateTransform(-cloud.Center.X, -cloud.Center.Y);
-            graphics.ScaleTransform(drawingScale.GetValueOrThrow(), drawingScale.GetValueOrThrow(), MatrixOrder.Append);
-            graphics.TranslateTransform(cloud.Center.X, cloud.Center.Y, MatrixOrder.Append);
-            graphics.Clear(cloudSettings.BackgroundColor);
-            foreach (var tag in cloud.Tags)
-            {
-                var font = new Font(tagSettings.TagFont, tag.Item1.FontSize);
-                graphics.DrawString(tag.Item1.Value, font, pen.Brush, tag.Item2.Location);
-            }
-        };
+        var bitmap = Result.Of(() => new Bitmap(imageWidth, imageHeight));
+        var graphics = bitmap.Then(Graphics.FromImage);
+        if (!graphics.IsSuccess)
+            return Result.Fail<Bitmap>(graphics.Error);
 
-        return bitmap.AsResult();
+        var scalingCloudResult = ScaleCloud(graphics.GetValueOrThrow(), cloud, imageWidth, imageHeight);
+        if(!scalingCloudResult.IsSuccess)
+            return Result.Fail<Bitmap>(scalingCloudResult.Error);
+
+        var drawingResult = DrawTags(
+            graphics.GetValueOrThrow(), cloud,
+            tagSettings.TagFont, cloudSettings.BackgroundColor,
+            pen.GetValueOrThrow());
+        if (!drawingResult.IsSuccess)
+            return Result.Fail<Bitmap>(drawingResult.Error);
+
+        return bitmap;
     }
 
-    public static Result<float> CalculateObjectDrawingScale(float width, float height, float imageWidth, float imageHeight)
+    private static Result<None> ScaleCloud(Graphics graphics, TagsCloud cloud, int width, int height)
+    {
+        var drawingScale = CalculateObjectDrawingScale(cloud.GetWidth(), cloud.GetHeight(), width, height);
+        if (!drawingScale.IsSuccess)
+            return Result.Fail<None>(drawingScale.Error);
+
+        var scalingResult = new Result<None>();
+        scalingResult = scalingResult.Then(res => graphics.TranslateTransform(-cloud.Center.X, -cloud.Center.Y));
+        scalingResult = scalingResult.Then(res => 
+        graphics.ScaleTransform(drawingScale.GetValueOrThrow(), drawingScale.GetValueOrThrow(), MatrixOrder.Append));
+        scalingResult = scalingResult.Then(res => graphics.TranslateTransform(cloud.Center.X, cloud.Center.Y, MatrixOrder.Append));
+
+        return scalingResult;
+    }
+
+    private static Result<float> CalculateObjectDrawingScale(float width, float height, float imageWidth, float imageHeight)
     {
         if (width <= 0 || height <= 0)
             return Result.Fail<float>("Scale can't be calculated of object with not positive width or height");
@@ -60,5 +76,19 @@ public class CloudDrawer : ICloudDrawer
         if (height * scale > imageHeight)
             heightScale = imageHeight / height - scaleAccuracy;
         return Math.Min(widthScale, heightScale);
+    }
+
+    private static Result<None> DrawTags(Graphics graphics, TagsCloud cloud, FontFamily tagFont, Color backgroundColor, Pen pen)
+    {
+        var drawingResult = new Result<None>();
+        drawingResult = drawingResult.Then(res => graphics.Clear(backgroundColor));
+        foreach (var tag in cloud.Tags)
+        {
+            var font = new Font(tagFont, tag.Item1.FontSize);
+            drawingResult = drawingResult.Then(res => 
+            graphics.DrawString(tag.Item1.Value, font, pen.Brush, tag.Item2.Location));
+        }
+
+        return drawingResult;
     }
 }
