4badad927e42c49f562abe1b5466d1de450e056c
daHil <ssocmain@gmail.com>
2024-02-04 17:22:50 +0500

    added tests for TagsBuilder

--- Tests/TagsBuilderTests.cs
+++ Tests/TagsBuilderTests.cs
@@ -1,4 +1,9 @@
 ﻿using FluentAssertions;
+using System.Drawing;
+using TagsCloudPainter.FormPointer;
+using TagsCloudPainter.Parser;
+using TagsCloudPainter.Settings.Cloud;
+using TagsCloudPainter.Settings.FormPointer;
 using TagsCloudPainter.Settings.Tag;
 using TagsCloudPainter.Tags;
 
@@ -17,9 +22,16 @@ public class TagsBuilderTests
     private TagsBuilder tagsBuilder;
 
     [Test]
+    public void Constructor_ShouldThrowArgumentNullException_WhenGivenNullITextSettings()
+    {
+        Assert.Throws<ArgumentNullException>(() => new TagsBuilder(null));
+    }
+
+    [Test]
     public void GetTags_ShouldReturnTagsWithGivenWords()
     {
         var words = new List<string> { "tag" };
+
         var tags = tagsBuilder.GetTags(words).GetValueOrThrow();
 
         tags[0].Value.Should().Be("tag");
@@ -29,6 +41,7 @@ public class TagsBuilderTests
     public void GetTags_ShouldReturnTagsWithDifferentValues()
     {
         var words = new List<string> { "tag", "tag" };
+
         var tags = tagsBuilder.GetTags(words).GetValueOrThrow();
 
         tags.Count.Should().Be(1);
@@ -38,8 +51,24 @@ public class TagsBuilderTests
     public void GetTags_ShouldReturnTagsWithCorrectCount()
     {
         var words = new List<string> { "tag", "tag" };
+
         var tags = tagsBuilder.GetTags(words).GetValueOrThrow();
 
         tags[0].Count.Should().Be(2);
+    }
+
+    private static IEnumerable<TestCaseData> FailingWords => new[]
+    {
+        new TestCaseData(null).SetName("WhenGivenNullWords"),
+        new TestCaseData(new List<string> { string.Empty }).SetName("WhenGivenWordsWithEmptyWord"),
+        new TestCaseData(new List<string> { null }).SetName("WhenGivenWordsWithNullWord"),
+    };
+
+    [TestCaseSource(nameof(FailingWords))]
+    public void GetTags_ShouldFail_WhenGivenNullWords(List<string> words)
+    {
+        var tags = tagsBuilder.GetTags(words);
+
+        Assert.That(tags.IsSuccess, Is.False);
     }
 }
