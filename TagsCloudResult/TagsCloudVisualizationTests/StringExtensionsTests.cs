cf7cdb40f0ae495fd360dabb9118c08ec5a5e2d4
tripples25 <92451706+tripples25@users.noreply.github.com>
2024-02-05 02:46:59 +0500

    test: added StringExtensions tests

---
+++ TagsCloudResult/TagsCloudVisualizationTests/StringExtensionsTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using TagsCloudVisualization.Extensions;
+
+namespace TagsCloudVisualizationTests;
+
+public class StringExtensionsTests
+{
+    [Test, TestCaseSource(nameof(GetAllWordsTestCases))]
+    public void GetAllWords_ReturnsAllWordsFromText_Correctly(string text, IEnumerable<string> expected)
+    {
+        text.GetAllWords().Should().BeEquivalentTo(expected);
+    }
+    
+    public static IEnumerable GetAllWordsTestCases
+    {
+        get
+        {
+            yield return new TestCaseData("", Array.Empty<string>());
+            yield return new TestCaseData("a a b c", new[] {"a", "a", "b", "c"});
+            yield return new TestCaseData("a]s \n asd", new[] {"a", "s", "asd"});
+            yield return new TestCaseData(",,,,,.....\\\\@#$%&?", Array.Empty<string>());
+            yield return new TestCaseData("0123456789", new[] {"0123456789"});
+        }
+    }
+}
