091b5e195c911da55fc5687acaf0059d0f42ec6d
luvairo-m <luvairo-228@yandex.ru>
2024-02-06 10:57:22 +0500

    Переработка TextAnalyzer: удаление try catch { } и добавление Result-логики.

--- TagsCloud/TextAnalysisTools/TextAnalyzer.cs
+++ TagsCloud/TextAnalysisTools/TextAnalyzer.cs
@@ -11,53 +11,60 @@ public class TextAnalyzer
 {
     public static Result<HashSet<WordTagGroup>> FillWithAnalysis(HashSet<WordTagGroup> wordGroups)
     {
-        try
+        return ResultExtensions
+               .Of(() => GetTextAnalysis(wordGroups))
+               .Then(analyses => FillGroups(wordGroups, analyses))
+               .ReplaceError(_ => "Something went wrong in words analyzing process!");
+    }
+
+    private static List<WordSummary> GetTextAnalysis(HashSet<WordTagGroup> wordGroups)
+    {
+        using var process = new Process();
+        process.StartInfo = new ProcessStartInfo
         {
-            using var process = new Process();
-            process.StartInfo = new ProcessStartInfo
+            FileName = "mystem",
+            Arguments = "-i --format=json",
+            UseShellExecute = false,
+            RedirectStandardInput = true,
+            RedirectStandardOutput = true
+        };
+
+        process.Start();
+
+        foreach (var group in wordGroups)
+            process.StandardInput.Write(group.WordInfo.Text + ' ');
+
+        process.StandardInput.Close();
+
+        var analyses = JsonSerializer
+            .Deserialize<List<WordSummary>>(process.StandardOutput.ReadToEnd());
+
+        process.WaitForExit();
+
+        return analyses;
+    }
+
+    private static HashSet<WordTagGroup> FillGroups(HashSet<WordTagGroup> wordGroups, IList<WordSummary> analyses)
+    {
+        var analysisIndex = 0;
+
+        foreach (var group in wordGroups)
+        {
+            var analysis = analyses[analysisIndex].Analyses.FirstOrDefault();
+
+            if (analysis == null)
             {
-                FileName = "mystem",
-                Arguments = "-i --format=json",
-                UseShellExecute = false,
-                RedirectStandardInput = true,
-                RedirectStandardOutput = true
-            };
-
-            process.Start();
-
-            foreach (var group in wordGroups)
-                process.StandardInput.Write(group.WordInfo.Text + ' ');
-
-            process.StandardInput.Close();
-
-            var analyses = JsonSerializer.Deserialize<List<WordSummary>>(process.StandardOutput.ReadToEnd());
-            process.WaitForExit();
-
-            var analysisIndex = 0;
-
-            foreach (var group in wordGroups)
+                group.WordInfo.IsRussian = false;
+            }
+            else
             {
-                var analysis = analyses[analysisIndex].Analyses.FirstOrDefault();
-
-                if (analysis == null)
-                {
-                    group.WordInfo.IsRussian = false;
-                }
-                else
-                {
-                    group.WordInfo.Infinitive = analysis.Infinitive;
-                    group.WordInfo.LanguagePart = analysis.LanguagePart;
-                }
-
-                analysisIndex++;
+                group.WordInfo.Infinitive = analysis.Infinitive;
+                group.WordInfo.LanguagePart = analysis.LanguagePart;
             }
 
-            return wordGroups;
+            analysisIndex++;
         }
-        catch
-        {
-            return ResultExtensions
-                .Fail<HashSet<WordTagGroup>>("Something went wrong in words analyzing process!");
-        }
+
+        return wordGroups;
     }
 }
