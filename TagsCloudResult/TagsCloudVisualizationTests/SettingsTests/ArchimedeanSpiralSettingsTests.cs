1ec244fdceec97c3bf6eb99b3e3e9aa6faad5263
tripples25 <92451706+tripples25@users.noreply.github.com>
2024-02-07 14:36:40 +0500

    test: add few tests on settings classes

---
+++ TagsCloudResult/TagsCloudVisualizationTests/SettingsTests/ArchimedeanSpiralSettingsTests.cs
@@ -0,0 +1,37 @@
+﻿using TagsCloudVisualization.Common.ResultOf;
+using TagsCloudVisualization.PointsProviders;
+
+namespace TagsCloudVisualizationTests.SettingsTests;
+
+public class ArchimedeanSpiralSettingsTests
+{
+    private const double CorrectDeltaAngle = 0.5;
+    private const double CorrectDistance = 1;
+
+    [Test]
+    public void Validate_ResultIsSuccess_WhenArgumentsOk()
+    {
+        var settings = new ArchimedeanSpiralSettings
+        {
+            Center = Point.Empty,
+            DeltaAngle = CorrectDeltaAngle,
+            Distance = CorrectDistance
+        };
+        
+        settings.Validate().IsSuccess.Should().BeTrue();
+    }
+    
+    [TestCase(0, CorrectDistance, TestName = "deltaAngle shouldn't equals zero")]
+    [TestCase(CorrectDeltaAngle, 0, TestName = "distance shouldn't equals zero")]
+    public void Validate_ResultIsFail_WhenArgumentsIncorrect(double deltaAngle, double distance)
+    {
+        var settings = new ArchimedeanSpiralSettings
+        {
+            Center = Point.Empty,
+            DeltaAngle = deltaAngle,
+            Distance = distance
+        };
+        
+        settings.Validate().IsSuccess.Should().BeFalse();
+    }
+}
---
+++ TagsCloudResult/TagsCloudVisualizationTests/SettingsTests/ImageSettingsTests.cs
@@ -0,0 +1,36 @@
+﻿using TagsCloudVisualization.Common;
+
+namespace TagsCloudVisualizationTests.SettingsTests;
+
+public class ImageSettingsTests
+{
+    private const int CorrectWidth = 100;
+    private const int CorrectHeight = 100;
+
+    [Test]
+    public void Validate_ResultIsSuccess_WhenArgumentsOk()
+    {
+        var settings = new ImageSettings
+        {
+            Width = CorrectWidth,
+            Height = CorrectHeight
+        };
+        
+        settings.Validate().IsSuccess.Should().BeTrue();
+    }
+    
+    [TestCase(0, CorrectHeight, TestName = "width shouldn't equals zero")]
+    [TestCase(-10, CorrectHeight, TestName = "width shouldn't less than zero")]
+    [TestCase(CorrectWidth, 0, TestName = "height shouldn't equals zero")]
+    [TestCase(CorrectWidth, -10, TestName = "height shouldn't less than zero")]
+    public void Validate_ResultIsFail_WhenArgumentsIncorrect(int width, int height)
+    {
+        var settings = new ImageSettings
+        {
+            Width = width,
+            Height = height
+        };
+        
+        settings.Validate().IsSuccess.Should().BeFalse();
+    }
+}
