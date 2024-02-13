fd59fe3bdc6f22d02bbca3f5b16b618ff05fd9e4
dragInSky <cronosdversus@mail.ru>
2024-02-13 17:19:24 +0500

    Save TryGetFont value

--- TagsCloudResult/FontFinder.cs
+++ TagsCloudResult/FontFinder.cs
@@ -5,12 +5,17 @@ namespace TagsCloudResult;
 
 public class FontFinder
 {
+    private static MyResult<FontFamily>? _tmp;
+    
     public static MyResult<FontFamily> TryGetFont(ApplicationArguments args)
     {
-        var res =
-            MyResult.Try(() => new FontCollection().Add(args.FontPath));
-        if (res.IsErr)
-            res.ReplaceError($"Font {args.FontPath} doesn't exist");
-        return res;
+        if (_tmp != null)
+            return _tmp;
+        
+        _tmp = MyResult.Try(() => new FontCollection().Add(args.FontPath));
+        if (_tmp.IsErr)
+            _tmp.ReplaceError($"Font {args.FontPath} doesn't exist");
+        
+        return _tmp;
     }
 }
