766e5c0b65cc22e7426ee89d0e94e47cce0f0aeb
Nikita_Trofimov <nikita174trofimov@gmail.com>
2024-02-19 15:42:30 +0500

    Рефакторинг: Вынес большую часть функционала из ConsoleApp.cs Выделил основную часть функционала из ConsoleApp.cs в отдельные файлы ProcessWord.cs и PathImage.cs, которые размещены в новой директории AppExtensions.

---
+++ TagsCloudContainer/App/AppExtensions/ProcessWord.cs
@@ -0,0 +1,34 @@
+﻿using TagsCloudContainer.FileReader;
+using TagsCloudContainer.WordProcessing;
+
+namespace TagsCloudContainer.App.AppExtensions;
+
+public class ProcessWord
+{
+    private Result<string> GetText(string filename)
+    {
+        if (!File.Exists(filename))
+        {
+            return Result.Fail<string>($"Файл не найден {filename}");
+        }
+        
+        var result = new FileReaderFactory()
+            .GetReader(filename)
+            .Then(x => x.GetTextFromFile(filename))
+            .RefineError("Не удается получить текст из файла");
+        return result;
+    }
+    
+    public List<Word> GetProcessWords(string text, string boringText, Settings settings)
+    {
+        var resultText = GetText(text).OnFail(Exit);
+        var resultBoringText = GetText(boringText).OnFail(Exit);
+        return new WordProcessor(settings).ProcessWords(resultText.Value, resultBoringText.Value);
+    }
+    
+    private void Exit(string text)
+    {
+        Console.WriteLine(text);
+        Environment.Exit(1);
+    }
+}
--- TagsCloudContainer/App/ConsoleApp.cs
+++ TagsCloudContainer/App/ConsoleApp.cs
@@ -1,74 +1,30 @@
-﻿using System.Drawing.Imaging;
+﻿using TagsCloudContainer.App.AppExtensions;
 using TagsCloudContainer.DrawRectangle;
-using TagsCloudContainer.FileReader;
 using TagsCloudContainer.WordProcessing;
 
 namespace TagsCloudContainer.App;
 
 public class ConsoleApp : IApp
 {
-    private  FileReaderFactory ReaderFactory { get; }
     private Settings Settings { get; }
-    private  WordProcessor Processor { get; }
     private  IDraw Draw { get; }
 
-    public ConsoleApp(FileReaderFactory readerFactory, Settings settings, WordProcessor processor,
-        IDraw draw)
+    public ConsoleApp(Settings settings, IDraw draw)
     {
-        ReaderFactory = readerFactory;
         Settings = settings;
-        Processor = processor;
         Draw = draw;
     }
     
     public Result<None> Run()
     {
-        var path = GetPathForSaveImage();
-        var words = GetProcessWords(Settings.File, Settings.BoringWordsFileName);
+        var path = new PathImage().GetPathForSaveImage(Settings);
+        var words = new ProcessWord().GetProcessWords(Settings.File, Settings.BoringWordsFileName, Settings);
         var bitmap = Draw.CreateImage(words).OnFail(Exit);
         bitmap.Value.Save(path);
         
         return new Result<None>(null);
     }
     
-    private string GetPathForSaveImage()
-    {
-        var imagesDirectory = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\Images";
-        var randomImagesSuffix = new Random().Next(1, 1000);
-        var imageFormat = GetImageFormat(Settings.ImageFormat).GetValueOrThrow();
-        var path = Path.Combine(imagesDirectory, $"Rectangle{randomImagesSuffix}.{imageFormat}");
-        return path;
-    }
-    private List<Word> GetProcessWords(string text, string boringText)
-    {
-        var resultText = GetText(text).OnFail(Exit);
-        var resultBoringText = GetText(boringText).OnFail(Exit);
-        return Processor.ProcessWords(resultText.Value, resultBoringText.Value);
-    }
-    private Result<ImageFormat> GetImageFormat(string imageFormat)
-    {
-        return imageFormat.ToLower() switch
-        {
-            "png" =>  ImageFormat.Png.Ok(),
-            "jpeg" => ImageFormat.Jpeg.Ok(),
-            _ => Result.Fail<ImageFormat>($"Неверный формат изображения: {imageFormat}")
-        };
-    }
-    
-    private Result<string> GetText(string filename)
-    {
-        if (!File.Exists(filename))
-        {
-            return Result.Fail<string>($"Файл не найден {filename}");
-        }
-        
-        var result = ReaderFactory
-            .GetReader(filename)
-            .Then(x => x.GetTextFromFile(filename))
-            .RefineError("Не удается получить текст из файла");
-        return result;
-    }
-
     private void Exit(string text)
     {
         Console.WriteLine(text);
