7bb1c0266aa73e4730957dcd5feaf772638abce5
Nikita_Trofimov <nikita174trofimov@gmail.com>
2024-02-26 12:07:16 +0500

    Refactor: поправил форматирование

--- TagsCloudContainer/Result/Result.cs
+++ TagsCloudContainer/Result/Result.cs
@@ -1,11 +1,11 @@
 ﻿namespace TagsCloudContainer.WordProcessing;
 
-public class Result <T>
+public class Result<T>
 {
     public string? Error { get; }
     public T Value { get; }
     public bool IsSuccess => Error == null;
-    
+
     public Result(string? error, T value = default(T))
     {
         Error = error;
